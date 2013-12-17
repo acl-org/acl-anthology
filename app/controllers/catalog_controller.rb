2732184fc286522d2f3a61b3eb0ea28a1af80d48
nglinh <nglinh2312@gmail.com>
2013-12-17 16:45:01 +0700

    Searchable, but only with case sensitive

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -51,21 +51,23 @@ class CatalogController < ApplicationController
     #
     # :show may be set to false if you don't want the facet to be drawn in the 
     # facet bar
-    config.add_facet_field 'format', :label => 'Format'
-    config.add_facet_field 'pub_date', :label => 'Publication Year', :single => true
-    config.add_facet_field 'subject_topic_facet', :label => 'Topic', :limit => 20 
-    config.add_facet_field 'language_facet', :label => 'Language', :limit => true 
-    config.add_facet_field 'lc_1letter_facet', :label => 'Call Number' 
-    config.add_facet_field 'subject_geo_facet', :label => 'Region' 
-    config.add_facet_field 'subject_era_facet', :label => 'Era'  
+    # config.add_facet_field 'format', :label => 'Format'
+    # config.add_facet_field 'pub_date', :label => 'Publication Year', :single => true
+    # config.add_facet_field 'subject_topic_facet', :label => 'Topic', :limit => 20 
+    # config.add_facet_field 'language_facet', :label => 'Language', :limit => true 
+    # config.add_facet_field 'lc_1letter_facet', :label => 'Call Number' 
+    # config.add_facet_field 'subject_geo_facet', :label => 'Region' 
+    # config.add_facet_field 'subject_era_facet', :label => 'Era'  
+    config.add_facet_field 'last_name', :label => "Last name"
+    config.add_facet_field 'first_name', :label => "First name"
 
-    config.add_facet_field 'example_pivot_field', :label => 'Pivot Field', :pivot => ['format', 'language_facet']
+    # config.add_facet_field 'example_pivot_field', :label => 'Pivot Field', :pivot => ['format', 'language_facet']
 
-    config.add_facet_field 'example_query_facet_field', :label => 'Publish Date', :query => {
-       :years_5 => { :label => 'within 5 Years', :fq => "pub_date:[#{Time.now.year - 5 } TO *]" },
-       :years_10 => { :label => 'within 10 Years', :fq => "pub_date:[#{Time.now.year - 10 } TO *]" },
-       :years_25 => { :label => 'within 25 Years', :fq => "pub_date:[#{Time.now.year - 25 } TO *]" }
-    }
+    # config.add_facet_field 'example_query_facet_field', :label => 'Publish Date', :query => {
+    #    :years_5 => { :label => 'within 5 Years', :fq => "pub_date:[#{Time.now.year - 5 } TO *]" },
+    #    :years_10 => { :label => 'within 10 Years', :fq => "pub_date:[#{Time.now.year - 10 } TO *]" },
+    #    :years_25 => { :label => 'within 25 Years', :fq => "pub_date:[#{Time.now.year - 25 } TO *]" }
+    # }
 
 
     # Have BL send all facet field names to Solr, which has been the default
@@ -75,32 +77,38 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display 
-    config.add_index_field 'title_display', :label => 'Title:' 
-    config.add_index_field 'title_vern_display', :label => 'Title:' 
-    config.add_index_field 'author_display', :label => 'Author:' 
-    config.add_index_field 'author_vern_display', :label => 'Author:' 
-    config.add_index_field 'format', :label => 'Format:' 
-    config.add_index_field 'language_facet', :label => 'Language:'
-    config.add_index_field 'published_display', :label => 'Published:'
-    config.add_index_field 'published_vern_display', :label => 'Published:'
-    config.add_index_field 'lc_callnum_display', :label => 'Call number:'
+    config.add_index_field 'last_name', :label => 'Last name:'
+    config.add_index_field 'first_name', :label => 'First name:'
+    # config.add_index_field 'title_display', :label => 'Title:' 
+    # config.add_index_field 'title_vern_display', :label => 'Title:' 
+    # config.add_index_field 'author_display', :label => 'Author:' 
+    # config.add_index_field 'author_vern_display', :label => 'Author:' 
+    # config.add_index_field 'format', :label => 'Format:' 
+    # config.add_index_field 'language_facet', :label => 'Language:'
+    # config.add_index_field 'published_display', :label => 'Published:'
+    # config.add_index_field 'published_vern_display', :label => 'Published:'
+    # config.add_index_field 'lc_callnum_display', :label => 'Call number:'
+    
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display 
-    config.add_show_field 'title_display', :label => 'Title:' 
-    config.add_show_field 'title_vern_display', :label => 'Title:' 
-    config.add_show_field 'subtitle_display', :label => 'Subtitle:' 
-    config.add_show_field 'subtitle_vern_display', :label => 'Subtitle:' 
-    config.add_show_field 'author_display', :label => 'Author:' 
-    config.add_show_field 'author_vern_display', :label => 'Author:' 
-    config.add_show_field 'format', :label => 'Format:' 
-    config.add_show_field 'url_fulltext_display', :label => 'URL:'
-    config.add_show_field 'url_suppl_display', :label => 'More Information:'
-    config.add_show_field 'language_facet', :label => 'Language:'
-    config.add_show_field 'published_display', :label => 'Published:'
-    config.add_show_field 'published_vern_display', :label => 'Published:'
-    config.add_show_field 'lc_callnum_display', :label => 'Call number:'
-    config.add_show_field 'isbn_t', :label => 'ISBN:'
+    config.add_show_field 'last_name', :label => 'Last name:'
+    config.add_show_field 'first_name', :label => 'First name:'
+    # config.add_show_field 'title_display', :label => 'Title:' 
+    # config.add_show_field 'title_vern_display', :label => 'Title:' 
+    # config.add_show_field 'subtitle_display', :label => 'Subtitle:' 
+    # config.add_show_field 'subtitle_vern_display', :label => 'Subtitle:' 
+    # config.add_show_field 'author_display', :label => 'Author:' 
+    # config.add_show_field 'author_vern_display', :label => 'Author:' 
+    # config.add_show_field 'format', :label => 'Format:' 
+    # config.add_show_field 'url_fulltext_display', :label => 'URL:'
+    # config.add_show_field 'url_suppl_display', :label => 'More Information:'
+    # config.add_show_field 'language_facet', :label => 'Language:'
+    # config.add_show_field 'published_display', :label => 'Published:'
+    # config.add_show_field 'published_vern_display', :label => 'Published:'
+    # config.add_show_field 'lc_callnum_display', :label => 'Call number:'
+    # config.add_show_field 'isbn_t', :label => 'ISBN:'
+    
 
     # "fielded" search configuration. Used by pulldown among other places.
     # For supported keys in hash, see rdoc for Blacklight::SearchFields
@@ -120,55 +128,68 @@ class CatalogController < ApplicationController
     # solr request handler? The one set in config[:default_solr_parameters][:qt],
     # since we aren't specifying it otherwise. 
     
-    config.add_search_field 'all_fields', :label => 'All Fields'
-    
+    # config.add_search_field 'all_fields', :label => 'All Fields'
+
+    config.add_search_field('first_name') do |field|
+      field.solr_parameters = { :q => 'first_name' }
+      field.solr_local_parameters = { 
+        :qf => 'first_name'
+      }
+    end
+
+    config.add_search_field('last_name') do |field|
+      field.solr_parameters = { :q => 'last_name' }
+      field.solr_local_parameters = { 
+        :qf => 'last_name'
+      }
+    end
 
     # Now we see how to over-ride Solr request handler defaults, in this
     # case for a BL "search field", which is really a dismax aggregate
     # of Solr search fields. 
     
-    config.add_search_field('title') do |field|
-      # solr_parameters hash are sent to Solr as ordinary url query params. 
-      field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
+    # config.add_search_field('title') do |field|
+    #   # solr_parameters hash are sent to Solr as ordinary url query params. 
+    #   field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
 
-      # :solr_local_parameters will be sent using Solr LocalParams
-      # syntax, as eg {! qf=$title_qf }. This is neccesary to use
-      # Solr parameter de-referencing like $title_qf.
-      # See: http://wiki.apache.org/solr/LocalParams
-      field.solr_local_parameters = { 
-        :qf => '$title_qf',
-        :pf => '$title_pf'
-      }
-    end
+    #   # :solr_local_parameters will be sent using Solr LocalParams
+    #   # syntax, as eg {! qf=$title_qf }. This is neccesary to use
+    #   # Solr parameter de-referencing like $title_qf.
+    #   # See: http://wiki.apache.org/solr/LocalParams
+    #   field.solr_local_parameters = { 
+    #     :qf => '$title_qf',
+    #     :pf => '$title_pf'
+    #   }
+    # end
     
-    config.add_search_field('author') do |field|
-      field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
-      field.solr_local_parameters = { 
-        :qf => '$author_qf',
-        :pf => '$author_pf'
-      }
-    end
+    # config.add_search_field('author') do |field|
+    #   field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
+    #   field.solr_local_parameters = { 
+    #     :qf => '$author_qf',
+    #     :pf => '$author_pf'
+    #   }
+    # end
     
     # Specifying a :qt only to show it's possible, and so our internal automated
     # tests can test it. In this case it's the same as 
     # config[:default_solr_parameters][:qt], so isn't actually neccesary. 
-    config.add_search_field('subject') do |field|
-      field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
-      field.qt = 'search'
-      field.solr_local_parameters = { 
-        :qf => '$subject_qf',
-        :pf => '$subject_pf'
-      }
-    end
+    # config.add_search_field('subject') do |field|
+    #   field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
+    #   field.qt = 'search'
+    #   field.solr_local_parameters = { 
+    #     :qf => '$subject_qf',
+    #     :pf => '$subject_pf'
+    #   }
+    # end
 
     # "sort results by" select (pulldown)
     # label in pulldown is followed by the name of the SOLR field to sort by and
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
-    config.add_sort_field 'score desc, pub_date_sort desc, title_sort asc', :label => 'relevance'
-    config.add_sort_field 'pub_date_sort desc, title_sort asc', :label => 'year'
-    config.add_sort_field 'author_sort asc, title_sort asc', :label => 'author'
-    config.add_sort_field 'title_sort asc, pub_date_sort desc', :label => 'title'
+    # config.add_sort_field 'score desc, pub_date_sort desc, title_sort asc', :label => 'relevance'
+    # config.add_sort_field 'pub_date_sort desc, title_sort asc', :label => 'year'
+    # config.add_sort_field 'author_sort asc, title_sort asc', :label => 'author'
+    # config.add_sort_field 'title_sort asc, pub_date_sort desc', :label => 'title'
 
     # If there are more than this many search results, no spelling ("did you 
     # mean") suggestion is offered.
