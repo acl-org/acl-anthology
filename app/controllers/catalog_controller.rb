6db629500ed87066e5d8a3d1a76e022191ebe1a4
nglinh <nglinh2312@gmail.com>
2013-12-26 15:23:11 +0700

    Added link to facet search on search result for author

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -53,13 +53,6 @@ class CatalogController < ApplicationController
     #
     # :show may be set to false if you don't want the facet to be drawn in the 
     # facet bar
-    # config.add_facet_field 'format', :label => 'Format'
-    # config.add_facet_field 'pub_date', :label => 'Publication Year', :single => true
-    # config.add_facet_field 'subject_topic_facet', :label => 'Topic', :limit => 20 
-    # config.add_facet_field 'language_facet', :label => 'Language', :limit => true 
-    # config.add_facet_field 'lc_1letter_facet', :label => 'Call Number' 
-    # config.add_facet_field 'subject_geo_facet', :label => 'Region' 
-    # config.add_facet_field 'subject_era_facet', :label => 'Era'
     config.add_facet_field 'title', :label => 'Title'
     config.add_facet_field 'author', :label => 'Author'
 
@@ -80,41 +73,14 @@ class CatalogController < ApplicationController
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display 
     config.add_index_field 'title', :label => 'Title:'
-    config.add_index_field 'author', :label => 'Author:'
-    # config.add_index_field 'last_name', :label => 'Last name:'
-    # config.add_index_field 'first_name', :label => 'First name:'
-    # config.add_index_field 'title_display', :label => 'Title:' 
-    # config.add_index_field 'title_vern_display', :label => 'Title:' 
-    # config.add_index_field 'author_display', :label => 'Author:' 
-    # config.add_index_field 'author_vern_display', :label => 'Author:' 
-    # config.add_index_field 'format', :label => 'Format:' 
-    # config.add_index_field 'language_facet', :label => 'Language:'
-    # config.add_index_field 'published_display', :label => 'Published:'
-    # config.add_index_field 'published_vern_display', :label => 'Published:'
-    # config.add_index_field 'lc_callnum_display', :label => 'Call number:'
+    config.add_index_field 'author', :label => 'Author:', :link_to_search => true
+    
     
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display 
     config.add_show_field 'title', :label => 'Title:'
     config.add_show_field 'author', :label => 'Author:'
-    # config.add_show_field 'last_name', :label => 'Last name:'
-    # config.add_show_field 'first_name', :label => 'First name:'
-    # config.add_show_field 'title', :label => 'Title:'
-    # config.add_show_field 'title_display', :label => 'Title:' 
-    # config.add_show_field 'title_vern_display', :label => 'Title:' 
-    # config.add_show_field 'subtitle_display', :label => 'Subtitle:' 
-    # config.add_show_field 'subtitle_vern_display', :label => 'Subtitle:' 
-    # config.add_show_field 'author_display', :label => 'Author:' 
-    # config.add_show_field 'author_vern_display', :label => 'Author:' 
-    # config.add_show_field 'format', :label => 'Format:' 
-    # config.add_show_field 'url_fulltext_display', :label => 'URL:'
-    # config.add_show_field 'url_suppl_display', :label => 'More Information:'
-    # config.add_show_field 'language_facet', :label => 'Language:'
-    # config.add_show_field 'published_display', :label => 'Published:'
-    # config.add_show_field 'published_vern_display', :label => 'Published:'
-    # config.add_show_field 'lc_callnum_display', :label => 'Call number:'
-    # config.add_show_field 'isbn_t', :label => 'ISBN:'
     
 
     # "fielded" search configuration. Used by pulldown among other places.
@@ -137,7 +103,12 @@ class CatalogController < ApplicationController
     
     # config.add_search_field 'all_fields', :label => 'All Fields'
 
-
+    config.add_search_field('title') do |field|
+      # field.solr_parameters = { :q => 'title'}
+      field.solr_local_parameters = {
+        :qf => '$title_qf'
+      }
+    end
 
     config.add_search_field('author') do |field|
       # field.solr_parameters = { :q => 'Person'}
@@ -146,12 +117,7 @@ class CatalogController < ApplicationController
       }
     end
 
-    config.add_search_field('title') do |field|
-      # field.solr_parameters = { :q => 'title'}
-      field.solr_local_parameters = {
-        :qf => '$title_qf'
-      }
-    end
+    
     # Now we see how to over-ride Solr request handler defaults, in this
     # case for a BL "search field", which is really a dismax aggregate
     # of Solr search fields. 
