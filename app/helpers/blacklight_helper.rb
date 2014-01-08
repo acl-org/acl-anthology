8fdde6c70c8698cd379d023283896f5c0f896c95
nglinh <nglinh2312@gmail.com>
2014-01-08 17:11:32 +0800

    removed debug statement

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -31,7 +31,6 @@ module BlacklightHelper
   opts[:label] ||= blacklight_config.index.show_link.to_sym
   label = render_document_index_label doc, opts
   if (doc[:paper_anthology][0] == "W" and doc[:paper_anthology][-2..-1] == "00") or doc[:paper_anthology][-3..-1] == "000"
-    puts "yolo"
     link_to label + " [VOLUME]", "/volumes/"+ doc[:volume_id]
   else
     link_to label, "/papers/" + doc[:id]
