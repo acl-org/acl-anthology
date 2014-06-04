7d4da61a38cd39107f989b6864e66494beed1043
Duong Ho Tuan <zamakkat@gmail.com>
2014-06-04 15:08:20 +0800

    changed to friendly links in search

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -6,24 +6,24 @@ module BlacklightHelper
   end
 
   def author_helper_method args
-  	args[:document][:author].map!.with_index { |author,i|
-  		link_to(author, "/people/" + args[:document][:author_id][i])
+  	args[:document][:author].map! { |author|
+  		link_to(author, person_path(author))
   	}
   end
 
   def volume_helper_method args
-  	link_to(args[:document][:volume_anthology], "/volumes/" + args[:document][:volume_id])
+  	link_to(args[:document][:volume_anthology], volume_path(args[:document][:volume_anthology]))
   end
 
   def sig_helper_method args
-  	args[:document][:sig_iden].map!.with_index { |sig,i|
-  		link_to(sig, "/sigs/" + args[:document][:sig_id][i])
+  	args[:document][:sig_iden].map! { |sig|
+  		link_to(sig, sig_path(sig))
   	}
   end
 
   def venue_helper_method args
-  	args[:document][:venue_name].map!.with_index { |venue,i|
-  		link_to(venue, "/venues/" + args[:document][:venue_id][i])
+  	args[:document][:venue_name].map! { |venue|
+  		link_to(venue, venue_path(venue))
   	}
   end
 
@@ -31,9 +31,9 @@ module BlacklightHelper
   opts[:label] ||= blacklight_config.index.show_link.to_sym
   label = render_document_index_label doc, opts
   if (doc[:paper_anthology][0] == "W" and doc[:paper_anthology][-2..-1] == "00") or doc[:paper_anthology][-3..-1] == "000"
-    link_to label + " [VOLUME]", "/volumes/"+ doc[:volume_id]
+    link_to label + " [VOLUME]", volume_path(doc[:volume_id])
   else
-    link_to label, "/papers/" + doc[:id]
+    link_to label, paper_path(doc[:id])
   end
 end
 
