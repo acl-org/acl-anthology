7164f90d817a39b0e2a20eb93e83200469a9e2d7
Duong Ho Tuan <zamakkat@gmail.com>
2014-07-31 19:03:01 +0800

    Fixed meta tags for Google Scholar. Still have to work on citation_journal_title

--- app/helpers/papers_helper.rb
+++ app/helpers/papers_helper.rb
@@ -1,2 +1,13 @@
 module PapersHelper
+	def get_citation_volume_from_title(title)
+		title_no_space = title.gsub(" ", "")
+		match_data = title_no_space.match(/Volume(?<volume>\d+)/)
+		return match_data && match_data[:volume]
+	end
+
+	def get_citation_issue_from_title(title)
+		title_no_space = title.gsub(" ", "")
+		match_data = title_no_space.match(/Number(?<volume>\d-\d)|Number(?<volume>\d)|Issue(?<volume>\d)/)
+		return match_data && match_data[:volume]
+	end
 end
