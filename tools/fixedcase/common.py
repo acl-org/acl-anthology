53abb4b52550df888dd011996da87a3ca59322fe
davidweichiang <dchiang@nd.edu>
2019-02-12 11:56:56 -0500

    Scripts for marking title words as fixed-case so BibTeX doesn't lowercase them

---
+++ tools/fixedcase/common.py
@@ -0,0 +1,62 @@
+import re
+import nltk.tokenize, nltk.corpus
+
+falselist = [w for w in nltk.corpus.words.words() if w.islower()]
+separators = [':', '-', '--', '\u2013', '---', '\u2014', '\u2015']
+
+def get_text(node):
+    result = []
+    def visit(node):
+        if node is not None:
+            if node.text is not None:
+                result.append(node.text)
+            for child in node:
+                visit(child)
+            if node.tail is not None:
+                result.append(node.tail)
+    visit(node)
+    return "".join(result)
+
+def tokenize(s):
+    tokens = []
+    # NLTK tokenizer uses PTB standard, which doesn't split on hyphens or slashes
+    for tok in nltk.tokenize.word_tokenize(s):
+        tokens.extend([t for t in re.split(r'[-–/]', tok) if t != ''])
+    return tokens
+    
+def fixedcase_word(w):
+    """Returns True if w should be fixed-case, False if not, None if unsure."""
+    if w.islower(): return False
+    if any(c.isupper() for c in w[1:]): return True
+
+def fixedcase_title(ws):
+    """Returns a list of bools: True if w should be fixed-case, False if
+    not, None if unsure."""
+    bs = []
+    for i, w in enumerate(ws):
+        b = fixedcase_word(w)
+        if b is None:
+            # In titles of the form "BLEU: a Method for Automatic Evaluation of Machine Translation,"
+            # where the first part is a single word, mark it as fixed-case
+            if len(ws) >= 2 and i == 0 and ws[1] in separators:
+                b = True
+        bs.append(b)
+    return bs
+        
+def replace_node(old, new):
+    old.clear()
+    old.tag = new.tag
+    old.attrib = new.attrib
+    old.text = new.text
+    old.extend(new)
+    old.tail = new.tail
+
+def append_text(node, text):
+    if len(node) == 0:
+        if node.text is None:
+            node.text = ""
+        node.text += text
+    else:
+        if node[-1].tail is None:
+            node[-1].tail = ""
+        node[-1].tail += text
---
+++ tools/fixedcase/protect.py
@@ -0,0 +1,53 @@
+import xml.etree.ElementTree as ET
+import sys
+from common import *
+
+def find_any(text, words, i=0):
+    for w in words:
+        j = text.find(w, i)
+        if j >= 0:
+            return j, j+len(w)
+    return None
+
+def protect(node, words):
+    newnode = ET.Element(node.tag, node.attrib)
+    def process(text):
+        if text is None: return
+        i = 0
+        span = find_any(text, words)
+        while span is not None:
+            append_text(newnode, text[i:span[0]])
+            p = ET.Element('fixed-case')
+            p.text = text[span[0]:span[1]]
+            newnode.append(p)
+            i = span[1]
+            span = find_any(text, words, i)
+        append_text(newnode, text[i:])
+    process(node.text)
+    for child in node:
+        newnode.append(protect(child, w))
+        process(child.tail)
+    newnode.tail = node.tail
+    return newnode
+
+if __name__ == "__main__":
+    truelist = []
+    for line in open("truelist"):
+        line = line.split('#')[0].strip()
+        if line == "": continue
+        truelist.append(line)
+    
+    infile, outfile = sys.argv[1:]
+    
+    tree = ET.parse(infile)
+    for paper in tree.getroot().findall('paper'):
+        for title in paper.findall('title'):
+            titletext = tokenize(get_text(title))
+            fixed = fixedcase_title(titletext)
+            if any(fixed):
+                print("old:", ET.tostring(title).decode('ascii').rstrip())
+                words = [w for w, b in zip(titletext, fixed) if b]
+                replace_node(title, protect(title, words))
+                print("new:", ET.tostring(title).decode('ascii').rstrip())
+
+    tree.write(outfile, encoding="UTF-8")
