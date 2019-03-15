a746e7532b885cb95c4cc9dbaa232f8892f5cb88
davidweichiang <dchiang@nd.edu>
2019-03-15 10:37:05 -0400

    improved diff_xml.py

--- bin/diff_xml.py
+++ bin/diff_xml.py
@@ -5,15 +5,16 @@ import copy
 
 unique_attributes = {'id'}
 elements = {'volume', 'paper', 'author', 'editor'}
-oldcolor = '\033[91m'
-newcolor = '\033[92m'
+oldcolor = '\033[91m\033[7m'
+newcolor = '\033[92m\033[7m'
 nocolor = '\033[0m'
+max_length = 65 # information in left margin is 14 chars
 
-def opentag(node, only_unique=False):
+def opentag(node):
     copy = etree.Element(node.tag)
     copy.text = "\n"
     for a, v in sorted(node.attrib.items()):
-        if not only_unique or a in unique_attributes:
+        if a in unique_attributes:
             copy.attrib[a] = v
     s = etree.tostring(copy, encoding=str)
     return s.splitlines()[0]
@@ -38,9 +39,91 @@ def tostring(node, max_length=None):
     else:
         return s
 
+def diff_strings(a, b, aloc, bloc, max_length):
+    sm = difflib.SequenceMatcher(a=a, b=b)
+    ops = sm.get_opcodes()
+
+    min_context = 15
+    def visit(ai, aj, bi, bj):
+        if aj-ai == bj-bi == 0: return
+
+        # Widen the windows by min_context
+        aa = max(0, ai-min_context)
+        az = min(len(a), aj+min_context)
+        ba = max(0, bi-min_context)
+        bz = min(len(b), bj+min_context)
+        
+        if az-aa <= max_length and bz-ba <= max_length:
+            # Widen more
+            aa = max(0, min(ai - (max_length-(aj-ai))//2, len(a)-max_length))
+            az = aa+max_length
+            ba = max(0, min(bi - (max_length-(bj-bi))//2, len(b)-max_length))
+            bz = ba+max_length
+            
+            # Print the differences all together
+            print('{:5d} {:5s} - '.format(aloc, ''), end='')
+            for op, ak, al, bk, bl in sm.get_opcodes():
+                if ai == ak and bi == bk: # first piece, print left context
+                    print(a[aa:ai], end='')
+                if ai <= ak <= al <= aj:
+                    if op == 'equal':
+                        print(a[ak:al], end='')
+                    else:
+                        print(oldcolor+a[ak:al]+nocolor, end='')
+                if al == aj and bl == bj: # last piece, print right context
+                    print(a[aj:az], end='')
+            print()
+
+            print('{:5s} {:5d} + '.format('', bloc), end='')
+            for op, ak, al, bk, bl in sm.get_opcodes():
+                if ai == ak and bi == bk:
+                    print(b[ba:bi], end='')
+                if ai <= ak <= al <= aj:
+                    if op == 'equal':
+                        print(b[bk:bl], end='')
+                    else:
+                        print(newcolor+b[bk:bl]+nocolor, end='')
+                if al == aj and bl == bj:
+                    print(b[bj:bz], end='')
+            print()
+
+        else:
+            # Try to split the strings in two (same way that difflib does it)
+            ak, bk, l = sm.find_longest_match(ai, aj, bi, bj)
+            if l > 0:
+                visit(ai, ak, bi, bk)
+                visit(ak+l, aj, bk+l, bj)
+            else:
+                # Just print the whole string with ... in the middle
+                aij = a[ai:aj]
+                if az-aa > max_length:
+                    l = (max_length - 3 - (ai-aa) - (az-aj))//2
+                    if l >= 0:
+                        aij = a[ai:ai+l] + '...' + a[aj-l:aj]
+                bij = b[bi:bj]
+                if bz-ba > max_length:
+                    l = (max_length - 3 - (bi-ba) - (bz-bj))//2
+                    if l >= 0:
+                        bij = b[bi:bi+l] + '...' + b[bj-l:bj]
+                
+                print('{:5d} {:5s} - {}'.format(aloc, '',
+                                                  a[aa:ai] +
+                                                  oldcolor + aij + nocolor +
+                                                  a[aj:az]))
+                print('{:5s} {:5d} + {}'.format('', bloc,
+                                                  b[ba:bi] +
+                                                  newcolor + bij + nocolor +
+                                                  b[bj:bz]))
+                
+    # strip outermost non-differences
+    diffs = [op for op in ops if op[0] != "equal"]
+    _, ai, _, bi, _ = diffs[0]
+    _, _, aj, _, bj = diffs[-1]
+    visit(ai, aj, bi, bj)
+
 def unified_diff(oldtree, newtree):
 
-    def visit(oldnode, newnode, indent=0):
+    def visit(oldnode, newnode):
         # Usually oldnode and newnode have the same tag, unless they are
         # both the root
 
@@ -48,37 +131,33 @@ def unified_diff(oldtree, newtree):
             # Just compare the nodes as strings
             oldstring = tostring(oldnode)
             newstring = tostring(newnode)
-            for op, oi, oj, ni, nj in difflib.SequenceMatcher(a=oldstring, b=newstring).get_opcodes():
-                if op != 'equal':
-                    c = (65-1-indent)//2
-                    oa = max(0, oi-c)
-                    oz = min(len(oldstring), oj+c)
-                    na = max(0, ni-c)
-                    nz = min(len(newstring), nj+c)
-                    print('{:5d} {:5s} - {}{}'.format(oldnode.sourceline, "", ' '*indent, oldstring[oa:oi]+oldcolor+oldstring[oi:oj]+nocolor+oldstring[oj:oz]))
-                    print('{:5s} {:5d} + {}{}'.format("", newnode.sourceline, ' '*indent, newstring[na:ni]+newcolor+newstring[ni:nj]+nocolor+newstring[nj:nz]))
+            if oldstring != newstring:
+                diff_strings(oldstring, newstring, oldnode.sourceline, newnode.sourceline, max_length)
                         
         else:
-            indent += 2
+            if (oldnode.text or "").strip():
+                print("warning: text at start of element at {}:{}".format(args.oldfile, oldnode.sourceline))
+            if (newnode.text or "").strip():
+                print("warning: text at start of element at {}:{}".format(args.newfile, newnode.sourceline))
+            for c in oldnode:
+                if (c.tail or "").strip():
+                    print("warning: text after element at {}:{}".format(args.oldfile, c.sourceline))
+            for c in newnode:
+                if (c.tail or "").strip():
+                    print("warning: text after element at {}:{}".format(args.newfile, c.sourceline))
 
-            # to do: compare text/tails
-            assert (oldnode.text or "").strip() == (newnode.text or "").strip() == ""
-            for oc, nc in zip(oldnode, newnode):
-                assert (oc.tail or "").strip() == (nc.tail or "").strip() == ""
+            oldchildren = sorted(oldnode, key=opentag)
+            newchildren = sorted(newnode, key=opentag)
 
-            def key(c): return opentag(c, True)
-            oldchildren = sorted(oldnode, key=key)
-            newchildren = sorted(newnode, key=key)
-
-            for op, oi, oj, ni, nj in difflib.SequenceMatcher(a=list(map(key, oldchildren)), b=list(map(key, newchildren))).get_opcodes():
+            for op, oi, oj, ni, nj in difflib.SequenceMatcher(a=list(map(opentag, oldchildren)), b=list(map(opentag, newchildren))).get_opcodes():
                 if op != 'equal':
                     for k in range(oi, oj):
-                        print('{:5d} {:5s} - {}{}'.format(oldchildren[k].sourceline, "", ' '*indent, oldcolor+tostring(oldchildren[k], 65-indent)+nocolor))
+                        print('{:5d} {:5s} - {}'.format(oldchildren[k].sourceline, "", oldcolor+tostring(oldchildren[k], max_length)+nocolor))
                     for k in range(ni, nj):
-                        print('{:5s} {:5d} + {}{}'.format("", newchildren[k].sourceline, ' '*indent, newcolor+tostring(newchildren[k], 65-indent)+nocolor))
+                        print('{:5s} {:5d} + {}'.format("", newchildren[k].sourceline, newcolor+tostring(newchildren[k], max_length)+nocolor))
                 else:
                     for ok, nk in zip(range(oi, oj), range(ni, nj)):
-                        visit(oldchildren[ok], newchildren[nk], indent)
+                        visit(oldchildren[ok], newchildren[nk])
 
     oldroot = oldtree.getroot()
     newroot = newtree.getroot()
@@ -89,9 +168,28 @@ if __name__ == "__main__":
     ap = argparse.ArgumentParser(description='Compare two XML files.')
     ap.add_argument('oldfile', help="first XML file")
     ap.add_argument('newfile', help="second XML file")
+    ap.add_argument('-w', '--width', type=int, default=80, help="terminal width")
     args = ap.parse_args()
+
+    max_length = args.width - 15
     
-    oldtree = etree.parse(args.oldfile)
-    newtree = etree.parse(args.newfile)
+    try:
+        oldtree = etree.parse(args.oldfile)
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        print("error: {}: {}".format(args.oldfile, e))
+        exit(1)
+        
+    try:
+        newtree = etree.parse(args.newfile)
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        print("error: {}: {}".format(args.newfile, e))
+        exit(1)
     
+    print('*****       - {}'.format(args.oldfile))
+    print('      ***** + {}'.format(args.newfile))
+
     unified_diff(oldtree, newtree)
