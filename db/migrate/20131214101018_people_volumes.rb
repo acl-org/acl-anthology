e0aa2ddb7794504db4362288b45b6623c89d6ab9
Marcel Bollmann <bollmann@linguistics.rub.de>
2019-03-16 09:55:02 +0100

    More cleanup

--- db/migrate/20131214101018_people_volumes.rb
+++
@@ -1,8 +0,0 @@
-class PeopleVolumes < ActiveRecord::Migration
-  def change
-  	create_table :people_volumes, :id => false do |t|
-  		t.integer :person_id
-    	t.integer :volume_id
-  	end
-  end
-end
--- db/migrate/20140713080248_add_layers_to_papers.rb
+++
@@ -1,5 +0,0 @@
-class AddLayersToPapers < ActiveRecord::Migration
-  def change
-    add_column :papers, :layers, :string
-  end
-end
