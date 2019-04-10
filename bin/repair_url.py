121c10dd8b446065779e727f55c5f5313a384f76
davidweichiang <dchiang@nd.edu>
2019-04-10 19:57:44 -0400

    script for repairing URLs

---
+++ bin/repair_url.py
@@ -0,0 +1,60 @@
+"""Repairs URLs in Anthology XML files.
+
+Usage: repair_url.py <infilename> <outfilename>
+
+To do:
+- check URLs of attachments
+- incorporate into a more general XML-fixing script
+
+"""
+
+import lxml.etree as etree
+import requests
+import sys
+
+def test_url(url):
+    #sys.stderr.write("retrieving {}: ".format(url))
+    r = requests.head(url, allow_redirects=True)
+    #sys.stderr.write("{}\n".format(r.status_code))
+    return r.status_code == requests.codes.ok
+
+filename = sys.argv[1]
+outfilename = sys.argv[2]
+tree = etree.parse(filename)
+volume = tree.getroot()
+for paper in volume.findall("paper"):
+    if 'href' in paper.attrib:
+        if not test_url(paper.attrib['href']):
+            sys.stderr.write('{}:{} removing href attribute: {}\n'.format(filename, paper.sourceline, paper.attrib['href']))
+            del paper.attrib['href']
+
+    href = paper.find('href')
+    if href is not None:
+        assert len(href) == 0
+        if not test_url(href.text):
+            sys.stderr.write('{}:{} removing href element: {}\n'.format(filename, href.sourceline, href.text))
+            paper.remove(href)
+    
+    anth_url = 'http://www.aclweb.org/anthology/{}-{:04d}'.format(volume.attrib['id'], int(paper.attrib['id']))
+    anth_url_good = test_url(anth_url)
+    
+    url = paper.find("url")
+    assert url is None or len(url) == 0
+    
+    if url is None:
+        if anth_url_good:
+            url = etree.Element('url')
+            url.text = anth_url
+            sys.stderr.write("{}:{} inserting url element: {}\n".format(filename, paper.sourceline, anth_url))
+            paper.append(url)
+        
+    else:
+        if anth_url_good and url.text != anth_url:
+            sys.stderr.write("{}:{} rewriting url: {} -> {}\n".format(filename, url.sourceline, url.text, anth_url))
+            url.text = anth_url
+
+        else:
+            sys.stderr.write("{}:{} removing url element because {} is bad\n".format(filename, url.sourceline, anth_url))
+            paper.remove(url)
+    
+tree.write(outfilename, encoding='UTF-8', xml_declaration=True, with_tail=True)
