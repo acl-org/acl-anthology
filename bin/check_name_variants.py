c89cb09a00aff086a1bf06b46f61af068987c048
davidweichiang <dchiang@nd.edu>
2019-04-12 19:48:15 -0400

    Trying out <first complete="...">

--- bin/check_name_variants.py
+++ bin/check_name_variants.py
@@ -1,14 +1,19 @@
 import yaml
 import sys
 import lxml.etree as etree
+import logging
 
 person_fields = {'canonical', 'variants', 'comment'}
 name_fields = {'first', 'last', 'papers'}
 
 def text(node):
     """Extract text from an XML node."""
-    if node is None: return ''
-    s = ''.join(node.itertext())
+    if node is None:
+        return ''
+    if 'complete' in node.attrib:
+        s = node.attrib['complete']
+    else:
+        s = ''.join(node.itertext())
     return ' '.join(s.split())
 
 def name(d):
@@ -20,7 +25,7 @@ if len(sys.argv) > 2:
         try:
             tree = etree.parse(xmlfilename)
         except:
-            print(xmlfilename)
+            logging.error("couldn't parse {}".format(xmlfilename))
             raise
         for paper in tree.getroot().findall('paper'):
             for person in paper.xpath('./author|./editor'):
@@ -40,14 +45,15 @@ for person in doc:
     assert isinstance(person['canonical'], dict), person
     assert set(person['canonical']).issubset(name_fields), person
     if names is not None and name(person['canonical']) not in names:
-        print('unused name', person['canonical'])
+        logging.warning('unused name: {}'.format(person['canonical']))
     dupes = {name(person['canonical'])}
     assert 'variants' in person, person
     assert isinstance(person['variants'], list), person
     for variant in person['variants']:
         assert set(variant).issubset(name_fields), person
         if names is not None and name(variant) not in names:
-            print('unused name', variant)
+            logging.warning('unused name: {}'.format(variant))
         assert name(variant) not in dupes, variant
         dupes.add(name(variant))
         
+print(yaml.dump(doc, allow_unicode=True))
