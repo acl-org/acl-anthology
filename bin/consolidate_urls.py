649617823b2012f7a9df1eb51d96fc4aa25947c2
Matt Post <post@cs.jhu.edu>
2019-05-10 11:06:53 -0400

    started volume work, postponing till #317

--- bin/consolidate_urls.py
+++ bin/consolidate_urls.py
@@ -15,18 +15,30 @@ import lxml.etree as etree
 import re
 import sys
 
+from repair_url import test_url, get_anth_url
+
 filename = sys.argv[1]
 outfilename = sys.argv[2]
 tree = etree.parse(filename)
 volume = tree.getroot()
-
 volume_id = volume.attrib['id']
+
+# Build list of volumes observed
+volumes = set()
+
 for paper in volume.findall("paper"):
     href = None
+
+    if volume_id.startswith('W'):
+        volumes.add(paper.attrib['id'][0:2])
+    else:
+        volumes.add(paper.attrib['id'][0:1])
+
     paper_id = int(paper.attrib['id'])
+    acl_id = '{}-{}'.format(volume_id, paper_id)
     if 'href' in paper.attrib:
         href = paper.attrib['href']
-        print('{}: removing href attribute {}'.format(paper_id, href), file=sys.stderr)
+        print('{}: removing href attribute {}'.format(acl_id, href), file=sys.stderr)
         del paper.attrib['href']
 
     if href and paper.find('href'):
@@ -37,7 +49,7 @@ for paper in volume.findall("paper"):
         assert len(href) == 0
         if not test_url(href.text):
             href = paper.find('href').text
-            print('{}: removing href element: {}'.format(paper_id, text), file=sys.stderr)
+            print('{}: removing href element: {}'.format(acl_id, text), file=sys.stderr)
             paper.remove(paper.find('href'))
 
     if href and paper.find('url'):
@@ -54,13 +66,40 @@ for paper in volume.findall("paper"):
     url = paper.find("url")
     if url is not None:
         if url.text != anth_url:
-            print("{}: rewriting url: {} -> {}".format(paper_id, url.text, anth_url), file=sys.stderr)
+            print("{}: rewriting url: {} -> {}".format(acl_id, url.text, anth_url), file=sys.stderr)
             url.text = anth_url
     else:
+        # For this initial conversion, we add the canonical URL
         url = etree.Element('url')
         url.text = anth_url
-        print("{}: inserting url element: {}".format(paper_id, anth_url), file=sys.stderr)
+        print("{}: inserting url element: {}".format(acl_id, anth_url), file=sys.stderr)
         paper.append(url)
 
+# Now look for volumes -- (postponed until after hierarchical change)
+# for id_ in sorted(volumes):
+#     url = etree.Element('url')
+#     url.text = get_anth_url(volume_id, int(id_), width=len(id_))
+#     if test_url(url.text):
+#         print("{}: inserting volume URL: {}".format(volume_id, url.text), file=sys.stderr)
+#         url.tail = '\n  '
+#         volume.insert(0, url)
+
+# https://stackoverflow.com/a/33956544
+# def indent(elem, level=0):
+#     i = "\n" + level*"  "
+#     if len(elem):
+#         if not elem.text or not elem.text.strip():
+#             elem.text = i + "  "
+#         if not elem.tail or not elem.tail.strip():
+#             elem.tail = i
+#         for elem in elem:
+#             indent(elem, level+1)
+#         if not elem.tail or not elem.tail.strip():
+#             elem.tail = i
+#     else:
+#         if level and (not elem.tail or not elem.tail.strip()):
+#             elem.tail = i
+
+# indent(volume)
 
 tree.write(outfilename, encoding='UTF-8', xml_declaration=True, with_tail=True)
