631b9706c8413162fd5657427edb1a01824b16cb
Matt Post <post@cs.jhu.edu>
2020-03-27 09:20:30 -0400

    made script more robust

--- bin/ingest_tsv.py
+++ bin/ingest_tsv.py
@@ -45,20 +45,21 @@ def download(remote_path, local_path):
         print(f"Creating directory {local_dir}", file=sys.stderr)
         os.makedirs(local_dir)
     try:
-        print(
-            f"-> Downloading file from {remote_path} to {local_path}", file=sys.stderr
-        )
+        print(f"-> Downloading file from {remote_path} to {local_path}", file=sys.stderr)
         with urllib.request.urlopen(remote_path) as url, open(
-                local_path, mode="wb"
+            local_path, mode="wb"
         ) as input_file_fh:
             input_file_fh.write(url.read())
     except ssl.SSLError:
-        raise Exception(f"Could not download {path}")
+        raise Exception(f"Could not download {remote_path} to {local_path}")
+    except urllib.error.HTTPError:
+        print(f"-> FAILED to download {remote_path}", file=sys.stderr)
+        return False
 
     return True
 
 
-def extract_pages(source_path , page_range, local_path):
+def extract_pages(source_path, page_range, local_path):
     if os.path.exists(local_path):
         print(f"{local_path} already exists, not re-extracting", file=sys.stderr)
         return True
@@ -68,7 +69,8 @@ def extract_pages(source_path , page_range, local_path):
     try:
         page_range = ' A'.join(page_range.split(','))
         print(
-            f"-> Extracting pages {page_range} from {source_path} to {local_path}", file=sys.stderr
+            f"-> Extracting pages {page_range} from {source_path} to {local_path}",
+            file=sys.stderr,
         )
         command = [f"pdftk A={source_path} cat {page_range} output {local_path}"]
         print(command, file=sys.stderr)
@@ -94,14 +96,20 @@ def main(args):
 
     # Create the metadata for the paper
     meta = None
-    for row in csv.DictReader(args.meta_file, delimiter="\t"):
+    for row in csv.DictReader(args.meta_file, delimiter=","):
         current_collection_id = f"{row['Year']}.{row['Conference code']}"
         if current_collection_id == collection_id:
             if row["Completed"] == "FALSE":
                 if args.force:
-                    print("Warning: Conference {collection_id} is not marked as completed.", file=sys.stderr)
+                    print(
+                        "Warning: Conference {collection_id} is not marked as completed.",
+                        file=sys.stderr,
+                    )
                 else:
-                    print("Warning: Conference {collection_id} is not marked as completed and no -f, can't ingest.", file=sys.stderr)
+                    print(
+                        "Warning: Conference {collection_id} is not marked as completed and no -f, can't ingest.",
+                        file=sys.stderr,
+                    )
                     sys.exit(1)
 
             meta = make_simple_element("meta", parent=volume)
@@ -117,12 +125,18 @@ def main(args):
                     if ", " in editor_name:
                         last, first = editor_name.split(", ")
                     else:
-                        first, last = ' '.join(editor_name.split()[:-1]), editor_name.split()[-1]
+                        first, last = (
+                            ' '.join(editor_name.split()[:-1]),
+                            editor_name.split()[-1],
+                        )
                     make_simple_element("first", first, parent=editor)
                     make_simple_element("last", last, parent=editor)
             break
     else:
-        print(f"Couldn't find conference code {collection_id} in 'Conference code' field of metadata file {args.meta_file.name}", file=sys.stderr)
+        print(
+            f"Couldn't find conference code {collection_id} in 'Conference code' field of metadata file {args.meta_file.name}",
+            file=sys.stderr,
+        )
         sys.exit(1)
 
     if not os.path.exists(collection_id):
@@ -135,30 +149,32 @@ def main(args):
         start_id = 1
 
     # Create entries for all the papers
-    for paperid, row in enumerate(csv.DictReader(args.tsv_file, delimiter='\t'), start_id):
+    for paperid, row in enumerate(
+        csv.DictReader(args.tsv_file, delimiter='\t'), start_id
+    ):
+        pages = row.get("Pagenumbers", None)
+
         if paperid == 0:
 
             title_text = row["Title"]
-            pages = row["Pagenumbers"]
-            try: 
+            try:
                 pdf = row["Pdf"]
             except KeyError:
                 pdf = None
-            paper = make_simple_element(
-                "frontmatter",
-                parent=volume
-            )
+            paper = make_simple_element("frontmatter", parent=volume)
             frontmatter = make_simple_element("frontmatter", parent=volume)
-            make_simple_element("pages", pages, parent=frontmatter)
+            if pages is not None:
+                make_simple_element("pages", pages, parent=frontmatter)
 
             url = f"{collection_id}-{volume_id}.{paperid}"
             pdf_local_path = os.path.join(collection_id, f"{url}.pdf")
             make_simple_element("url", url, parent=frontmatter)
             if not pdf is None:
-                download(pdf, pdf_local_path)
+                if not download(pdf, pdf_local_path):
+                    pdf = None
             else:
                 pdf_pages = row["pages in pdf"]
-                extract_pages(args.proceedings, pdf_pages, pdf_local_path) 
+                extract_pages(args.proceedings, pdf_pages, pdf_local_path)
             continue
 
         title_text = row["Title"]
@@ -169,11 +185,7 @@ def main(args):
         except KeyError:
             pdf = None
 
-        paper = make_simple_element(
-            "paper",
-            attrib={"id": str(paperid)},
-            parent=volume
-        )
+        paper = make_simple_element("paper", attrib={"id": str(paperid)}, parent=volume)
 
         make_simple_element("title", title_text, parent=paper)
         for author_name in author_list:
@@ -187,18 +199,26 @@ def main(args):
             make_simple_element("first", first, parent=author)
             make_simple_element("last", last, parent=author)
 
-        if "Pagenumbers" in row:
-            pages = row["Pagenumbers"]
+        if pages is not None:
             make_simple_element("pages", pages, parent=paper)
 
         url = f"{collection_id}-{volume_id}.{paperid}"
-        pdf_local_path = os.path.join(args.anthology_files_path, collection_id, f"{url}.pdf")
-        make_simple_element("url", url, parent=paper)
+        pdf_local_path = os.path.join(
+            args.anthology_files_path, collection_id, f"{url}.pdf"
+        )
         if not pdf is None:
-            download(pdf, pdf_local_path)
-        else:
+            if not download(pdf, pdf_local_path):
+                url = None
+
+        elif "pages in pdf" in row:
             pdf_pages = row["pages in pdf"]
-            extract_pages(args.proceedings, pdf_pages, pdf_local_path) 
+            extract_pages(args.proceedings, pdf_pages, pdf_local_path)
+
+        else:
+            url = None
+
+        if url is not None:
+            make_simple_element("url", url, parent=paper)
 
         if "Abstract" in row:
             make_simple_element("abstract", row["Abstract"], parent=paper)
@@ -206,11 +226,7 @@ def main(args):
         if "Presentation" in row and row["Presentation"] != "":
             extension = row["Presentation"].split(".")[-1]
             filename = f"{collection_id}-{volume_id}.{paperid}.Presentation.{extension}"
-            make_simple_element(
-                "attachment",
-                filename,
-                attrib={"type": "presentation"}
-            )
+            make_simple_element("attachment", filename, attrib={"type": "presentation"})
             download(row["Presentation"], os.path.join(collection_id, filename))
 
         # Normalize
@@ -220,24 +236,28 @@ def main(args):
     indent(tree.getroot())
 
     # Write the file to disk: acl-anthology/data/xml/{collection_id}.xml
-    collection_file = os.path.join(
-        args.anthology, "data", "xml", f"{collection_id}.xml"
-    )
-    tree.write(
-        collection_file, encoding="UTF-8", xml_declaration=True, with_tail=True
-    )
+    collection_file = os.path.join(args.anthology, "data", "xml", f"{collection_id}.xml")
+    tree.write(collection_file, encoding="UTF-8", xml_declaration=True, with_tail=True)
 
 
 if __name__ == '__main__':
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument('tsv_file', type=argparse.FileType("r"))
-    parser.add_argument('meta_file', type=argparse.FileType("r"),
-                        help="Path to conference metadata file")
-    parser.add_argument('--anthology', default=f"{os.environ.get('HOME')}/code/acl-anthology",
-                        help="Path to Anthology repo (cloned from https://github.com/acl-org/acl-anthology)")
-    parser.add_argument('--anthology-files-path', default=f"{os.environ.get('HOME')}/anthology-files/pdf",
-                        help="Path to Anthology files (Default: ~/anthology-files")
+    parser.add_argument(
+        'meta_file', type=argparse.FileType("r"), help="Path to conference metadata file"
+    )
+    parser.add_argument(
+        '--anthology',
+        default=f"{os.environ.get('HOME')}/code/acl-anthology",
+        help="Path to Anthology repo (cloned from https://github.com/acl-org/acl-anthology)",
+    )
+    parser.add_argument(
+        '--anthology-files-path',
+        default=f"{os.environ.get('HOME')}/anthology-files/pdf",
+        help="Path to Anthology files (Default: ~/anthology-files",
+    )
 
     parser.add_argument('--proceedings', help="Path to PDF with conference proceedings")
     parser.add_argument('--frontmatter', action="store_true")
