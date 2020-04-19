cb62132ff18e3ed1f72ebfe072723b58fc547a47
Nathan Schneider <nathan.schneider@georgetown.edu>
2020-04-19 00:44:38 -0400

    code formatting (make autofix)

--- bin/fixedcase/common.py
+++ bin/fixedcase/common.py
@@ -6,11 +6,14 @@ import nltk.tokenize, nltk.corpus
 falselist = set([w for w in nltk.corpus.words.words() if w.islower()])
 separators = [":", "--", "\u2013", "---", "\u2014", "\u2015"]
 
+
 def is_hyphen(s):
     return s in ("-", "–")
 
+
 def no_hyphens(ws):
     return tuple(w for w in ws if not is_hyphen(w))
+
 
 def get_text(node):
     result = []
@@ -46,15 +49,18 @@ def fixedcase_word(w, truelist=None, falselist=None, allcaps=False):
     if falselist is not None and w in falselist:
         return False
 
-def fixedcase_prefix(ws, truelist=None, phrase_truelist=None, falselist=None, allcaps=False):
+
+def fixedcase_prefix(
+    ws, truelist=None, phrase_truelist=None, falselist=None, allcaps=False
+):
     """Returns a list of 1 or more bools: True if some prefix of the tuple 'ws' should be fixed-case,
     False if not, None if unsure."""
     # phrase_truelist is sorted in descending order by phrase length
     if phrase_truelist is not None:
-        for n,truelist_bin in phrase_truelist:
+        for n, truelist_bin in phrase_truelist:
             if ws[:n] in truelist_bin:
-                return [True]*n
-            if len(no_hyphens(ws))>=n and no_hyphens(ws)[:n] in truelist_bin:
+                return [True] * n
+            if len(no_hyphens(ws)) >= n and no_hyphens(ws)[:n] in truelist_bin:
                 # no hyphens in truelist entries
                 bs = []
                 i = 0
@@ -64,12 +70,22 @@ def fixedcase_prefix(ws, truelist=None, phrase_truelist=None, falselist=None, al
                     else:
                         bs.append(True)
                         i += 1
-                        if i==n:
+                        if i == n:
                             break
                 return bs
-    return [fixedcase_word(ws[0], truelist=truelist, falselist=falselist, allcaps=allcaps)]
+    return [
+        fixedcase_word(ws[0], truelist=truelist, falselist=falselist, allcaps=allcaps)
+    ]
 
-def fixedcase_title(ws, truelist=None, phrase_truelist=None, amodifiers=None, ndescriptors=None, falselist=None):
+
+def fixedcase_title(
+    ws,
+    truelist=None,
+    phrase_truelist=None,
+    amodifiers=None,
+    ndescriptors=None,
+    falselist=None,
+):
     """Returns a list of bools: True if w should be fixed-case, False if
     not, None if unsure."""
 
@@ -79,26 +95,39 @@ def fixedcase_title(ws, truelist=None, phrase_truelist=None, amodifiers=None, nd
     bs = []
     ws = tuple(ws)
     i = 0
-    while i<len(ws):
-        b = fixedcase_prefix(ws[i:], truelist=truelist, phrase_truelist=phrase_truelist, falselist=falselist, allcaps=allcaps)
-        if i==0:
-            if b[0] is None and len(ws)>=2 and ws[1] in separators:
+    while i < len(ws):
+        b = fixedcase_prefix(
+            ws[i:],
+            truelist=truelist,
+            phrase_truelist=phrase_truelist,
+            falselist=falselist,
+            allcaps=allcaps,
+        )
+        if i == 0:
+            if b[0] is None and len(ws) >= 2 and ws[1] in separators:
                 # In titles of the form "BLEU: a Method for Automatic Evaluation of Machine Translation,"
                 # where the first part is a single word, mark it as fixed-case
                 b[0] = True
-        elif b[0] and amodifiers and ws[i-1] in amodifiers: # e.g. North America
+        elif b[0] and amodifiers and ws[i - 1] in amodifiers:  # e.g. North America
             bs[-1] = True
-        elif b[0] and is_hyphen(ws[i-1]) and amodifiers and ws[i-2] in amodifiers:
+        elif b[0] and is_hyphen(ws[i - 1]) and amodifiers and ws[i - 2] in amodifiers:
             bs[-2] = True
-        elif not b[0] and bs[-1] and ndescriptors and ws[i-1] in ndescriptors:
+        elif not b[0] and bs[-1] and ndescriptors and ws[i - 1] in ndescriptors:
             # "<name> <ndescriptor>", e.g. Columbia University
             b[0] = True
-        elif b[0] and ndescriptors and i>=2 and ws[i-1]=="of" and ws[i-2] in ndescriptors:
+        elif (
+            b[0]
+            and ndescriptors
+            and i >= 2
+            and ws[i - 1] == "of"
+            and ws[i - 2] in ndescriptors
+        ):
             # "<ndescriptor> of <name>", e.g. University of Edinburgh
             bs[-2] = True
         bs.extend(b)
         i += len(b)
     return bs
+
 
 def replace_node(old, new):
     old.clear()
--- bin/fixedcase/protect.py
+++ bin/fixedcase/protect.py
@@ -32,28 +32,32 @@ def protect_recurse(node, recased):
 
     def process(text, rc):
         i = 0
-        for upper, chars in itertools.groupby(rc[:len(text)], lambda c: c.isupper()):
+        for upper, chars in itertools.groupby(rc[: len(text)], lambda c: c.isupper()):
             charstr = "".join(chars)
             if upper:
                 p = ET.Element("fixed-case")
                 p.text = charstr
                 newnode.append(p)
             else:
-                append_text(newnode, text[i:i+len(charstr)])
+                append_text(newnode, text[i : i + len(charstr)])
 
-            assert text[i:i+len(charstr)].lower()==charstr.lower(),(i,text,charstr)
+            assert text[i : i + len(charstr)].lower() == charstr.lower(), (
+                i,
+                text,
+                charstr,
+            )
             i += len(charstr)
 
     if node.text:
         process(node.text, recased)
-        recased = recased[len(node.text):]
+        recased = recased[len(node.text) :]
     for child in node:
         protected_child = protect_recurse(child, recased)
-        recased = recased[len(protected_child.text):]
+        recased = recased[len(protected_child.text) :]
         newnode.append(protected_child)
         if child.tail:
             process(child.tail, recased)
-            recased = recased[len(child.tail):]
+            recased = recased[len(child.tail) :]
 
     return newnode
 
@@ -61,16 +65,22 @@ def protect_recurse(node, recased):
 def protect(node):
     rawtext = get_text(node).strip()
     text = tokenize(rawtext)
-    fixed = fixedcase_title(text, truelist=truelist, phrase_truelist=phrase_truelist,
-        amodifiers=amodifiers, ndescriptors=ndescriptors, falselist=falselist)
+    fixed = fixedcase_title(
+        text,
+        truelist=truelist,
+        phrase_truelist=phrase_truelist,
+        amodifiers=amodifiers,
+        ndescriptors=ndescriptors,
+        falselist=falselist,
+    )
     if any(fixed):
-        #words = [w for w, b in zip(text, fixed) if b]
+        # words = [w for w, b in zip(text, fixed) if b]
         recased = ''
         for w, b in zip(text, fixed):
             recased += w if b else w.lower()
-            if len(rawtext)>len(recased) and rawtext[len(recased)] == ' ':
+            if len(rawtext) > len(recased) and rawtext[len(recased)] == ' ':
                 recased += ' '
-        assert rawtext.lower()==recased.lower(),(rawtext,recased)
+        assert rawtext.lower() == recased.lower(), (rawtext, recased)
         newnode = protect_recurse(node, recased)
         newnode.tail = node.tail  # tail of top level is not protected
         replace_node(node, newnode)
@@ -93,13 +103,44 @@ for line in open(phrase_truelist_file):
     if line == "":
         continue
     toks = tuple(tokenize(line))
-    assert no_hyphens(toks)==toks,f'Phrasal truelist entries should not contain hyphens: {line}'
-    phrase_truelist[len(toks)].add(toks)    # group phrases by number of tokens
-phrase_truelist = sorted(phrase_truelist.items(), reverse=True) # bins sorted by phrase length
-amodifiers = ('North', 'South', 'East', 'West', 'Northeast', 'Northwest', 'Southeast', 'Southwest', 'Central',
-             'Northern', 'Southern', 'Eastern', 'Western', 'Northeastern', 'Northwestern', 'Southeastern', 'Southwestern',
-             'Modern', 'Ancient') # use subsequent word to determine fixed-case. will miss hyphenated modifiers (e.g. South-East)
-ndescriptors = ('Bay', 'Coast', 'Gulf', 'Island', 'Isle', 'Lake', 'Republic', 'University') # use preceding word to determine fixed-case
+    assert (
+        no_hyphens(toks) == toks
+    ), f'Phrasal truelist entries should not contain hyphens: {line}'
+    phrase_truelist[len(toks)].add(toks)  # group phrases by number of tokens
+phrase_truelist = sorted(
+    phrase_truelist.items(), reverse=True
+)  # bins sorted by phrase length
+amodifiers = (
+    'North',
+    'South',
+    'East',
+    'West',
+    'Northeast',
+    'Northwest',
+    'Southeast',
+    'Southwest',
+    'Central',
+    'Northern',
+    'Southern',
+    'Eastern',
+    'Western',
+    'Northeastern',
+    'Northwestern',
+    'Southeastern',
+    'Southwestern',
+    'Modern',
+    'Ancient',
+)  # use subsequent word to determine fixed-case. will miss hyphenated modifiers (e.g. South-East)
+ndescriptors = (
+    'Bay',
+    'Coast',
+    'Gulf',
+    'Island',
+    'Isle',
+    'Lake',
+    'Republic',
+    'University',
+)  # use preceding word to determine fixed-case
 
 if __name__ == "__main__":
     infile, outfile = sys.argv[1:]
