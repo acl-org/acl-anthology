3431e91eecfcf07b9d698eeec7f002fdfeeda775
Daniel Gildea <gildea>
2020-07-14 15:35:01 -0400

    reformat

--- bin/likely_name_split.py
+++ bin/likely_name_split.py
@@ -22,10 +22,10 @@ from anthology import Anthology
 from anthology.people import PersonName
 
 # counts of how often each name appears
-first_count = defaultdict(lambda:0)	# "Maria" "Victoria"
-first_full_count = defaultdict(lambda:0)# "Maria Victoria"
-last_count = defaultdict(lambda:0)	# "van" "den" "Bosch"
-last_full_count = defaultdict(lambda:0)	# "van den Bosch"
+first_count = defaultdict(lambda: 0)  # "Maria" "Victoria"
+first_full_count = defaultdict(lambda: 0)  # "Maria Victoria"
+last_count = defaultdict(lambda: 0)  # "van" "den" "Bosch"
+last_full_count = defaultdict(lambda: 0)  # "van den Bosch"
 first_total = 0
 last_total = 0
 
@@ -38,7 +38,7 @@ def count_names(anthology):
     for person in anthology.people.personids():
         name = anthology.people.get_canonical_name(person)
         num_papers = len(anthology.people.get_papers(person)) + 0.0
-        #print(name.last, ", ", name.first, num_papers)
+        # print(name.last, ", ", name.first, num_papers)
         for w in name.first.split(" "):
             first_count[w] += num_papers
         first_full_count[name.first] += num_papers
@@ -49,27 +49,28 @@ def count_names(anthology):
         last_full_count[name.last] += num_papers
         last_total += num_papers
 
+
 # takes "Maria Victoria Lopez Gonzalez"
 # returns ("Lopez Gonzalez", "Maria Victoria")
 # uses counts of words in first and last names in current database
 def best_split(name):
     words = name.split(" ")
     best_score = -inf
-    best = ("","")
+    best = ("", "")
     # loop over possible split points between first/last
-    for i in range(1,len(words)):	# at least one word in each part
+    for i in range(1, len(words)):  # at least one word in each part
         first = " ".join(words[0:i])
         last = " ".join(words[i:])
         # max of log prob of "Maria Victoria" and
         # log prob of "Maria" + log prob of "Victoria"
-        first_probs = [ log((first_count[x]+0.01)/first_total)
-                        for x in words[0:i] ]
-        first_score = max ( log((first_full_count[first]+0.000001)/first_total),
-                            sum(first_probs) )
-        last_probs = [ log((last_count[x]+0.01)/last_total)
-                       for x in words[i:] ]
-        last_score = max ( log((last_full_count[last]+0.000001)/last_total),
-                           sum(last_probs) )
+        first_probs = [log((first_count[x] + 0.01) / first_total) for x in words[0:i]]
+        first_score = max(
+            log((first_full_count[first] + 0.000001) / first_total), sum(first_probs)
+        )
+        last_probs = [log((last_count[x] + 0.01) / last_total) for x in words[i:]]
+        last_score = max(
+            log((last_full_count[last] + 0.000001) / last_total), sum(last_probs)
+        )
 
         if first_score + last_score > best_score:
             best_score = first_score + last_score
@@ -100,6 +101,6 @@ if __name__ == "__main__":
         best = best_split(name.first + " " + name.last)
 
         # if current split does not match our prediction
-        if not ( best[0] == name.last and best[1] == name.first ):
+        if not (best[0] == name.last and best[1] == name.first):
             # print suggested replacement
-            print( name.last, ",", name.first, "  ==>  ", best[0], ",", best[1])
+            print(name.last, ",", name.first, "  ==>  ", best[0], ",", best[1])
