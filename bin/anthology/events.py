e2e951040d60a01cec5f103df0f9e887e3e23f1f
Matt Post <mattpost@microsoft.com>
2022-12-26 07:50:53 -0600

    fixed reference

--- bin/anthology/events.py
+++ bin/anthology/events.py
@@ -95,7 +95,7 @@ class EventIndex:
                         and not url.startswith("http")
                         and not url.startswith("/")
                     ):
-                        url = data.EVENT_LOCATION_TEMPLATE.format(event_id, url)
+                        url = EVENT_LOCATION_TEMPLATE.format(event_id, url)
 
                     self.events[event_id]["links"].append({name.capitalize(): url})
 
