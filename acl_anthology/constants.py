7607e2f9232eff8351e98a07023dc83b0e04e3e8
Marcel Bollmann <marcel@bollmann.me>
2023-06-03 13:07:57 +0200

    Add first parts of volume metadata parsing

--- acl_anthology/constants.py
+++ acl_anthology/constants.py
@@ -12,5 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# ruff: noqa: F401
-from .volumeindex import VolumeIndex
+UNKNOWN_INGEST_DATE = "1900-01-01"
