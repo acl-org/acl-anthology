7348bee85462acda301712a87d58840bb9ee8b04
Marcel Bollmann <marcel@bollmann.me>
2023-08-06 11:49:02 +0200

    Begin functionality to load the variant list

--- acl_anthology/people/index.py
+++ acl_anthology/people/index.py
@@ -14,12 +14,21 @@
 
 from __future__ import annotations
 
+from pathlib import Path
 from typing import TYPE_CHECKING
+import yaml
 
-from . import Person
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader  # type: ignore
+
+from . import Person, Name
 
 if TYPE_CHECKING:
     from ..anthology import Anthology
+
+VARIANTS_FILE = "yaml/name_variants.yaml"
 
 
 class PersonIndex:
@@ -31,9 +40,46 @@ class PersonIndex:
 
     def __init__(self, parent: Anthology) -> None:
         self.parent = parent
+
         self.people: dict[str, Person] = {}
+        """A mapping of IDs to [Person][acl_anthology.people.person.Person] instances."""
+
         self.is_built = False
+        """A flag indicating whether the index has been constructed."""
 
     def build_index(self) -> None:
         """Load the entire Anthology data and build an index of persons."""
         raise NotImplementedError()
+
+    @staticmethod
+    def generate_id(name: Name) -> str:
+        """Generates and returns an ID from the given name.
+
+        This **intentionally doesn't guarantee uniqueness** of the generated ID.
+        If two names generate identical IDs with this method, we assume they
+        refer to the same person.  This happens e.g. when there are missing
+        accents in one version, or when we have an inconsistent first/last split
+        for multiword names.  These cases have in practice always referred to
+        the same person.
+        """
+        return name.slugify()
+
+    def _load_variant_list(self) -> None:
+        filename = self.parent.datadir / Path(VARIANTS_FILE)
+        with open(filename, "r") as f:
+            variant_list = yaml.load(f, Loader=Loader)
+        for entry in variant_list:
+            # Every entry must have a "canonical" name
+            canonical = Name.from_dict(entry["canonical"])
+            # If it doesn't define an ID, we have to create one
+            if (pid := entry.get("id")) is None:
+                pid = self.generate_id(canonical)
+            # Parse all the variant names, and make sure canonical stays at index 0
+            names = [canonical] + [
+                Name.from_dict(var) for var in entry.get("variants", [])
+            ]
+            # Now we can create a new person from this entry...
+            person = Person(id=pid, names=names)
+            # ...and add it to the index
+            self.people[pid] = person
+            # TODO: process the "similar" key
