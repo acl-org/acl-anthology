b7408afc82dddb362006332aace953a6b220cf84
Matt Post <post@cs.jhu.edu>
2023-12-07 19:31:12 -0500

    More EMNLP 2023 volumes (#2905)
    Main, Findings, and a handful of workshops

--- bin/anthology_check.py
+++ bin/anthology_check.py
@@ -3,6 +3,11 @@
 """
 Last updated 2023-11-23 by Matt Post.
 
+TODO:
+- Ensure no LaTeX in titles (e.g., \emph{...})
+- If there is only a single name, it should be the last name, not the first
+- Clean TeX from abstracts, too (e.g., 2023.findings-emnlp.439)
+
 Sanity check for ACLPUB2 submissions to the ACL Anthology.
 If this script doesn't pass, you're not ready to submit!
 The expected format (default from aclpub2) is as follows:
