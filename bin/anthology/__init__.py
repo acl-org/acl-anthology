e0d0d225ae25f580dea09748f7e25f8d2ed09b91
Marcel Bollmann <marcel@bollmann.me>
2024-11-03 16:22:04 +0100

    Add deprecation warning to old library

--- bin/anthology/__init__.py
+++ bin/anthology/__init__.py
@@ -3,3 +3,11 @@ from .anthology import Anthology
 from .people import PersonName
 from .papers import Paper
 from .volumes import Volume
+
+import warnings
+
+warnings.warn(
+    """Your code uses the legacy Anthology library.  Consider migrating to acl-anthology-py <https://acl-anthology-py.readthedocs.io/>""",
+    FutureWarning,
+    stacklevel=2,
+)
