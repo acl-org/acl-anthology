c566f19e4b659a2c918bd77f39d4c2de82dc9205
Marcel Bollmann <marcel@bollmann.me>
2024-12-26 14:42:14 +0100

    Update copyright years

--- bin/create_bibtex.py
+++ bin/create_bibtex.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright 2019 Marcel Bollmann <marcel@bollmann.me>
+# Copyright 2019-2024 Marcel Bollmann <marcel@bollmann.me>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
