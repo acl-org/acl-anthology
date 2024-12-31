2a3a638f339408b228ce756197ac053108546e76
Marcel Bollmann <marcel@bollmann.me>
2024-12-31 15:46:04 +0100

    Refactor create_bib.py to compile full BibTeX files from data

--- bin/create_bib.py
+++ bin/create_bib.py
@@ -38,9 +38,13 @@ from rich.progress import track
 import shutil
 import subprocess
 
-from acl_anthology import Anthology, config
+from acl_anthology import config
+from acl_anthology.utils.ids import infer_year
 from acl_anthology.utils.logging import setup_rich_logging
 from create_hugo_data import check_directory
+
+
+DECODER = msgspec.json.Decoder()
 
 
 def convert_bibtex(bibtex):
@@ -59,31 +63,27 @@ def convert_bibtex(bibtex):
     return mods.strip(" \ufeff\r\n"), endf.strip(" \ufeff\r\n")
 
 
-def create_bibtex(anthology, builddir, clean=False) -> None:
-    """Creates .bib files for all papers.
-    """
+def create_bibtex(builddir, clean=False) -> None:
+    """Create full Anthology BibTeX files."""
     trgdir = f"{builddir}/data-export"
-    if not check_directory("{}/papers".format(trgdir), clean=clean):
-        return
-    if not check_directory("{}/volumes".format(trgdir), clean=clean):
-        return
 
-    log.debug("Creating BibTeX files for all papers...")
+    # gzip.open(
+    #        f"{builddir}/data-export/anthology+abstracts.bib.gz", "wt", encoding="utf-8"
+    #    ) as file_anthology_with_abstracts,
+    # ):
+
     with (
         open(
-            "{}/anthology.bib".format(trgdir), "wt", encoding="utf-8"
+            f"{builddir}/data-export/anthology.bib", "wt", encoding="utf-8"
         ) as file_anthology_raw,
         gzip.open(
-            "{}/anthology.bib.gz".format(trgdir), "wt", encoding="utf-8"
+            f"{builddir}/data-export/anthology.bib.gz", "wt", encoding="utf-8"
         ) as file_anthology,
-        gzip.open(
-            "{}/anthology+abstracts.bib.gz".format(trgdir), "wt", encoding="utf-8"
-        ) as file_anthology_with_abstracts,
     ):
         # Add a header to each consolidated bibfile
-        for outfh in file_anthology_raw, file_anthology, file_anthology_with_abstracts:
+        for outfh in file_anthology_raw, file_anthology:
             print(
-                f"% https://aclanthology.org/{Path(outfh.name).name} generated on {datetime.date.today().isoformat()}\n",
+                f"% {config.url_prefix}/{Path(outfh.name).name} generated on {datetime.date.today().isoformat()}\n",
                 file=outfh,
             )
 
@@ -92,83 +92,91 @@ def create_bibtex(anthology, builddir, clean=False) -> None:
             "@string{acl = {Association for Computational Linguistics}}",
             file=file_anthology_raw,
         )
-        print("@string{anth = {https://aclanthology.org/}}", file=file_anthology_raw)
+        print(f"@string{{anth = {{{config.url_prefix}/}}}}", file=file_anthology_raw)
         print(file=file_anthology_raw)
 
-        for volume in track(
+        for volume_file in track(
             sorted(
-                anthology.volumes(), key=lambda vol: (vol.year, vol.full_id), reverse=True
+                Path(f"{builddir}/data-export/volumes").glob("*.bib"),
+                key=lambda p: (infer_year(p.stem), p.stem),
+                reverse=True,
             ),
-            description="Creating BibTeX files...",
+            description="Creating anthology.bib.gz...          ",
         ):
             # reset this each time
             abbrev = None
+            volume_id = volume_file.stem
 
-            volume_dir = trgdir
-            if not os.path.exists(volume_dir):
-                os.makedirs(volume_dir)
-            with open(
-                "{}/volumes/{}.bib".format(trgdir, volume.full_id), "w"
-            ) as file_volume:
-                for i, paper in enumerate(volume.values(), 1):
-                    with open(
-                        "{}/{}.bib".format(volume_dir, paper.full_id), "w"
-                    ) as file_paper:
-                        contents = paper.to_bibtex(with_abstract=True)
-                        print(contents, file=file_paper)
-                        print(contents, file=file_anthology_with_abstracts)
+            with open(volume_file, "r") as f:
+                bibtex = f.read()
+            print(bibtex, file=file_anthology)
 
-                        concise_contents = paper.to_bibtex()
-                        print(concise_contents, file=file_volume)
-                        print(concise_contents, file=file_anthology)
+            # Space saver (https://github.com/acl-org/acl-anthology/issues/3016) for the
+            # uncompressed consolidated bibfile.
+            # Replace verbose text with abbreviations to get the file under 50 MB for Overleaf
+            concise_contents = bibtex.replace(
+                'publisher = "Association for Computational Linguistics",',
+                "publisher = acl,",
+            )
+            concise_contents = re.sub(
+                rf'url = "{config.url_prefix}/(.*)"',
+                r"url = anth # {\1}",
+                concise_contents,
+            )
 
-                        # Space saver (https://github.com/acl-org/acl-anthology/issues/3016) for the
-                        # uncompressed consolidated bibfile.
-                        # Replace verbose text with abbreviations to get the file under 50 MB for Overleaf
-                        concise_contents = concise_contents.replace(
-                            'publisher = "Association for Computational Linguistics",',
-                            "publisher = acl,",
-                        )
-                        concise_contents = re.sub(
-                            r'url = "https://aclanthology.org/(.*)"',
-                            r"url = anth # {\1}",
-                            concise_contents,
-                        )
+            # Abbreviate the booktitle by extracting it and printing it before
+            # the first entry in each volume
+            if concise_contents.startswith("@proceedings"):
+                # Grab the title string and create the alias
+                first_bibkey_comp = re.match(
+                    r'@proceedings{([a-z0-9]*)-', concise_contents
+                ).group(1)
+                abbrev = f"{first_bibkey_comp.upper()}:{infer_year(volume_id)}:{volume_id.split('-')[-1]}"
+                try:
+                    booktitle = re.match(
+                        r"@proceedings{[a-z0-9-]*,\n    title = \"(.*)\",",
+                        concise_contents,
+                    ).group(1)
+                    print(
+                        f"@string{{{abbrev} = {{{booktitle}}}}}",
+                        file=file_anthology_raw,
+                    )
+                except AttributeError:
+                    log.warning(f"Could not find title for {volume_id}")
+                    abbrev = None
 
-                        # Abbreviate the booktitle by extracting it and printing it before
-                        # the first entry in each volume
-                        if concise_contents.startswith("@proceedings"):
-                            # Grab the title string and create the alias
-                            abbrev = (
-                                f"{volume.venue_ids[0].upper()}:{volume.year}:{volume.id}"
-                            )
-                            try:
-                                booktitle = re.search(
-                                    r"    title = \"(.*)\",", concise_contents
-                                ).group(1)
-                                print(
-                                    f"@string{{{abbrev} = {{{booktitle}}}}}",
-                                    file=file_anthology_raw,
-                                )
-                            except AttributeError:
+                if abbrev is not None and "booktitle" in concise_contents:
+                    # substitute the alias for the booktitle
+                    concise_contents = re.sub(
+                        r"    booktitle = (\".*\"),",
+                        f"    booktitle = {abbrev},",
+                        concise_contents,
+                    )
 
-                                log.warning(f"Could not find title for {volume.full_id}")
-                                abbrev = None
+                # Remove whitespace to save space and keep things under 50 MB
+                concise_contents = re.sub(r",\n +", ",", concise_contents)
+                concise_contents = re.sub(r"  and\n +", " and ", concise_contents)
+                concise_contents = re.sub(r",\n}", "}", concise_contents)
 
-                        if abbrev is not None and "booktitle" in concise_contents:
-                            # substitute the alias for the booktitle
-                            concise_contents = re.sub(
-                                r"    booktitle = (\".*\"),",
-                                f"    booktitle = {abbrev},",
-                                concise_contents,
-                            )
+                print(concise_contents, file=file_anthology_raw)
 
-                        # Remove whitespace to save space and keep things under 50 MB
-                        concise_contents = re.sub(r",\n +", ",", concise_contents)
-                        concise_contents = re.sub(r"  and\n +", " and ", concise_contents)
-                        concise_contents = re.sub(r",\n}", "}", concise_contents)
+    with gzip.open(
+        f"{builddir}/data-export/anthology+abstracts.bib.gz", "wt", encoding="utf-8"
+    ) as file_anthology_with_abstracts:
+        for collection_file in track(
+            sorted(
+                Path(f"{builddir}/data/papers").glob("*.json"),
+                key=lambda p: (infer_year(p.stem), p.stem),
+                reverse=True,
+            ),
+            description="Creating anthology+abstracts.bib.gz...",
+        ):
+            with open(collection_file, "rb") as f:
+                data = DECODER.decode(f.read())
 
-                        print(concise_contents, file=file_anthology_raw)
+            for entry in data.values():
+                if bibtex := entry.get("bibtex"):
+                    print(bibtex, file=file_anthology_with_abstracts)
 
 
 if __name__ == "__main__":
