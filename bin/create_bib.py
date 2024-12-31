3b9252b638e2a728181e3d47a0ad29de9c13f188
Marcel Bollmann <marcel@bollmann.me>
2024-12-31 18:09:06 +0100

    Use multiprocessing for MODS/Endnote conversion

--- bin/create_bib.py
+++ bin/create_bib.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Usage: create_bib.py [--builddir=DIR] [-c] [--debug]
+"""Usage: create_bib.py [--builddir=DIR] [-c] [--max-workers=N] [--debug]
 
 Creates anthology.bib files and MODS/Endnote formats for all papers in the Hugo directory.
 
@@ -23,9 +23,11 @@ Options:
   --builddir=DIR           Directory with build files; used both for reading and writing. [default: {scriptdir}/../build/]
   --debug                  Output debug-level log messages.
   -c, --clean              Delete existing files in target directory before generation.
+  -n, --max-workers=N      Maximum number of subprocesses that will be spawned.
   -h, --help               Display this helpful text.
 """
 
+import concurrent.futures
 import datetime
 from docopt import docopt
 import gzip
@@ -35,19 +37,25 @@ import msgspec
 from pathlib import Path
 import re
 from rich.progress import track
+import shutil
 import subprocess
 
 from acl_anthology import config
 from acl_anthology.utils.ids import infer_year
 from acl_anthology.utils.logging import setup_rich_logging
+from create_hugo_data import make_progress
 
 
-DECODER = msgspec.json.Decoder()
-ENCODER = msgspec.json.Encoder()
+BIB2XML = None
+XML2END = None
 
 
 def create_bibtex(builddir, clean=False) -> None:
-    """Create full Anthology BibTeX files."""
+    """Create full Anthology BibTeX files.
+
+    Requires volume bib files from create_hugo_data.py (for file without abstracts).
+    Requires data files from create_hugo_data.py (for file with abstracts).
+    """
     with (
         open(
             f"{builddir}/data-export/anthology.bib", "wt", encoding="utf-8"
@@ -77,7 +85,7 @@ def create_bibtex(builddir, clean=False) -> None:
                 key=lambda p: (infer_year(p.stem), p.stem),
                 reverse=True,
             ),
-            description="Creating anthology.bib.gz...          ",
+            description="Create anthology.bib.gz...  ",
         ):
             # reset this each time
             abbrev = None
@@ -145,10 +153,10 @@ def create_bibtex(builddir, clean=False) -> None:
                 key=lambda p: (infer_year(p.stem), p.stem),
                 reverse=True,
             ),
-            description="Creating anthology+abstracts.bib.gz...",
+            description="       +abstracts.bib.gz... ",
         ):
             with open(collection_file, "rb") as f:
-                data = DECODER.decode(f.read())
+                data = msgspec.json.decode(f.read())
 
                 # bibtex = "\n".join(entry["bibtex"] for entry in data.values() if "bibtex" in entry)
                 # print(bibtex, file=file_anthology_with_abstracts)
@@ -158,46 +166,61 @@ def create_bibtex(builddir, clean=False) -> None:
                         print(bibtex, file=file_anthology_with_abstracts)
 
 
-def convert_bibtex(builddir):
-    """Convert BibTeX into other bibliographic formats, and add them to the data files."""
-    for collection_file in track(
-        list(Path(f"{builddir}/data/papers").glob("*.json")),
-        description="Converting to MODS & Endnote...       ",
-    ):
-        with open(collection_file, "rb") as f:
-            data = DECODER.decode(f.read())
+def convert_bibtex(builddir, max_workers=None):
+    """Convert BibTeX into other bibliographic formats, and add them to the data files.
 
-        # for entry in data.values():
-        #    if bibtex := entry.get("bibtex"):
-        #        entry["mods"], entry["endf"] = get_mods_and_endf(bibtex)
+    Requires data files from create_hugo_data.py.
+    """
+    files = list(Path(f"{builddir}/data/papers").glob("*.json"))
 
-        entries = [entry for entry in data.values() if entry.get("bibtex")]
-        if not entries:
-            continue
+    with make_progress() as progress:
+        task = progress.add_task("Convert to MODS & Endnote...", total=len(files))
 
-        bibtex = "\n".join(entry["bibtex"] for entry in entries)
-        mods_batch, endf_batch = batch_convert_to_mods_and_endf(
-            bibtex, collection_file.name
-        )
-        assert len(entries) == len(mods_batch) == len(endf_batch)
-        for entry, mods, endf in zip(entries, mods_batch, endf_batch):
-            entry["mods"] = mods
-            entry["endf"] = endf
+        with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor:
+            futures = [executor.submit(convert_collection_file, file) for file in files]
+            for _ in concurrent.futures.as_completed(futures):
+                progress.update(task, advance=1)
 
-        with open(collection_file, "wb") as f:
-            f.write(ENCODER.encode(data))
+
+def convert_collection_file(collection_file):
+    """Read a single collection data file, convert its BibTeX entries to MODS and Endnote formats, and save those back into the file.
+
+    Important:
+        This function should not rely on global objects, as it will be executed concurrently for different files with multiprocessing.
+    """
+
+    with open(collection_file, "rb") as f:
+        data = msgspec.json.decode(f.read())
+
+    entries = [entry for entry in data.values() if entry.get("bibtex")]
+    if not entries:
+        return
+
+    bibtex = "\n".join(entry["bibtex"] for entry in entries)
+    mods_batch, endf_batch = batch_convert_to_mods_and_endf(bibtex, collection_file.name)
+    assert len(entries) == len(mods_batch) == len(endf_batch)
+    for entry, mods, endf in zip(entries, mods_batch, endf_batch):
+        entry["mods"] = mods
+        entry["endf"] = endf
+
+    with open(collection_file, "wb") as f:
+        f.write(msgspec.json.encode(data))
 
 
 def batch_convert_to_mods_and_endf(bibtex, context):
+    """Convert a BibTeX string with multiple entries to MODS and Endnote.
+
+    Relies on bibutils to perform the conversion, then returns a list with the individual converted entries.
+    """
     mods = subprocess.run(
-        ["/usr/bin/bib2xml", "-nt"],
+        [BIB2XML, "-nt"],
         input=bibtex,
         capture_output=True,
         text=True,
     )
     log.debug(f"{context}: {mods.stderr.strip()}")
     endf = subprocess.run(
-        ["/usr/bin/xml2end"],
+        [XML2END],
         input=mods.stdout,
         capture_output=True,
         text=True,
@@ -207,9 +230,9 @@ def batch_convert_to_mods_and_endf(bibtex, context):
     mods_header, *mods_entries = re.split(r"<mods ", mods.stdout)
     mods_header = mods_header.lstrip("\ufeff")
     mods_footer = "</modsCollection>\n"
-    mods_batch = [f"{mods_header}{entry}{mods_footer}" for entry in mods_entries[:-1]] + [
-        f"{mods_header}{mods_entries[:-1]}"
-    ]
+    mods_batch = [
+        f"{mods_header}<mods {entry}{mods_footer}" for entry in mods_entries[:-1]
+    ] + [f"{mods_header}<mods {mods_entries[-1]}"]
 
     endf_batch = endf.stdout.strip("\ufeff\r\n").split("\n\n")
 
@@ -227,7 +250,15 @@ if __name__ == "__main__":
     log_level = log.DEBUG if args["--debug"] else log.INFO
     tracker = setup_rich_logging(level=log_level)
 
+    max_workers = int(args["--max-workers"]) if args["--max-workers"] else None
+    if (BIB2XML := shutil.which("bib2xml")) is None:
+        log.error("bib2xml not found; please install bibutils for MODS XML conversion")
+    if (XML2END := shutil.which("xml2end")) is None:
+        log.error("xml2end not found; please install bibutils for Endnote conversion")
+
     create_bibtex(args["--builddir"], clean=args["--clean"])
-    convert_bibtex(args["--builddir"])
+    if BIB2XML and XML2END:
+        convert_bibtex(args["--builddir"], max_workers=max_workers)
+
     if tracker.highest >= log.ERROR:
         exit(1)
