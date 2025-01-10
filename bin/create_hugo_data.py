9d79fd5e20556d6c5835a446b56b4d61686efddc
Marcel Bollmann <marcel@bollmann.me>
2025-01-10 18:16:27 +0100

    Fix co-author and venue sorting, fixes #4383

--- bin/create_hugo_data.py
+++ bin/create_hugo_data.py
@@ -347,16 +347,12 @@ def export_people(anthology, outdir, dryrun):
                 "full": cname.as_full(),
                 "slug": person_id,
                 "papers": [paper.full_id for paper in papers],
-                "coauthors": sorted(
-                    anthology.people.find_coauthors_counter(
-                        person, include_volumes=False
-                    ).most_common()
-                ),
-                "venues": sorted(
-                    Counter(
-                        venue for paper in papers for venue in paper.venue_ids
-                    ).most_common()
-                ),
+                "coauthors": anthology.people.find_coauthors_counter(
+                    person, include_volumes=False
+                ).most_common(),
+                "venues": Counter(
+                    venue for paper in papers for venue in paper.venue_ids
+                ).most_common(),
             }
             if len(person.names) > 1:
                 data["variant_entries"] = []
