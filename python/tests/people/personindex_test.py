347111908431af23db6f83417f48b7bf69df53b4
Marcel Bollmann <marcel@bollmann.me>
2025-08-03 16:14:52 +0200

    Remove tests for get_or_create_person, fix remaining ones

--- python/tests/people/personindex_test.py
+++ python/tests/people/personindex_test.py
@@ -27,50 +27,29 @@ def index_with_full_anthology(anthology):
     return PersonIndex(anthology)
 
 
-def test_load_variant_list(index):
-    index._load_variant_list()
+def test_load_people_index(index):
+    index._load_people_index()
     index.is_data_loaded = True
     for pid in (
-        "pranav-a",
-        "pranav-anand",
-        "yang-liu-edinburgh",
+        "emily-prudhommeaux",
+        "steven-krauwer",
         "yang-liu-icsi",
         "yang-liu-ict",
         "yang-liu-microsoft",
-        "steven-krauwer",
     ):
         assert pid in index
 
 
-def test_load_variant_list_correct_variants(index):
-    index._load_variant_list()
+def test_load_people_index_registers_names(index):
+    index._load_people_index()
     index.is_data_loaded = True
-    n1 = Name("Susan", "Armstrong")
-    n2 = Name("Susan", "Warwick")
+    n1 = Name("Steven", "Krauwer")
+    n2 = Name("S.", "Krauwer")
     assert n1 in index.name_to_ids
     assert n2 in index.name_to_ids
     pid = index.name_to_ids[n1]
     assert pid == index.name_to_ids[n2]
     assert pid[0] in index
-
-
-def test_load_variant_list_correct_ids(index):
-    # If no explicit ID is defined, the ID should be based on the canonical
-    # name in the variants list
-    index._load_variant_list()
-    index.is_data_loaded = True
-    n1 = Name("Susan", "Warwick-Armstrong")
-    pid = index.name_to_ids[n1]
-    assert pid == ["susan-armstrong"]
-
-
-def test_load_variant_find_people_single_name(index):
-    # People with a single name should correctly be found
-    index._load_variant_list()
-    index.is_data_loaded = True
-    n1 = Name(None, "Srinivas")
-    pid = index.name_to_ids[n1]
-    assert pid == ["srinivas-bangalore"]
 
 
 def test_add_person(index):
@@ -87,89 +66,16 @@ def test_add_person(index):
         index.add_person(Person("yang-liu", index.parent))
 
 
-def test_get_or_create_person_with_id(index):
-    index.is_data_loaded = True
-    ns1 = NameSpecification(Name("Yang", "Liu"), id="yang-liu-icsi")
-    ns2 = NameSpecification(Name("Y.", "Liu"), id="yang-liu-icsi")
-    with pytest.raises(NameIDUndefinedError):
-        index.get_or_create_person(ns1)
-    index._load_variant_list()
-    person1 = index.get_or_create_person(ns1)
-    assert person1.id == "yang-liu-icsi"
-    person2 = index.get_or_create_person(ns2)
-    assert person1 is person2
-    assert person1 is index["yang-liu-icsi"]
-    assert person1.has_name(Name("Yang", "Liu"))
-    assert person1.has_name(Name("Y.", "Liu"))
-
-
-def test_get_or_create_person_new_person(index):
-    index.is_data_loaded = True
-    ns1 = NameSpecification(Name("Yang", "Liu"))
-    ns2 = NameSpecification(Name("Yang", "Liu"), affiliation="University of Edinburgh")
-    person1 = index.get_or_create_person(ns1)
-    assert person1.has_name(Name("Yang", "Liu"))
-    person2 = index.get_or_create_person(ns2)
-    assert person1 is person2
-    assert person1 is index[person1.id]
-
-
-def test_get_or_create_person_new_person_disallowed(index):
-    ns1 = NameSpecification(Name("Yang", "Liu"))
-    with pytest.raises(NameIDUndefinedError):
-        index.get_or_create_person(ns1, create=False)
-
-
-def test_get_or_create_person_with_ambiguous_name(index):
-    index._load_variant_list()
-    ns1 = NameSpecification(Name("Yang", "Liu"))
-    ns2 = NameSpecification(Name("Yang", "Liu"), id="yang-liu-icsi")
-    with pytest.raises():
-        index.get_or_create_person(ns1)
-    person = index.get_or_create_person(ns2)
-    assert person.id == "yang-liu-icsi"
-
-
-def test_get_or_create_person_with_name_merging(index):
-    ns1 = NameSpecification(Name("John", "Neumann"))
-    ns2 = NameSpecification(Name("Jöhn", "Néumänn"))
-    person1 = index.get_or_create_person(ns1)
-    person2 = index.get_or_create_person(ns2)
-    assert person1 is person2
-    assert person2.has_name(ns1.name)
-    assert person2.has_name(ns2.name)
-    assert person2.canonical_name == ns2.name
-
-
-def test_get_or_create_person_with_explicit_canonical_name(index):
-    index._load_variant_list()
-    # This name is defined as canonical in the variants list
-    ns1 = NameSpecification(Name("Emily", "Prud’hommeaux"))
-    # This one is not, but scores higher according to our heuristics
-    ns2 = NameSpecification(Name("Emily", "Prud’Hommeaux"))
-    assert (
-        ns2.name.score() > ns1.name.score()
-    ), "This test assumes that `ns2` will score higher than `ns1`."
-    person1 = index.get_or_create_person(ns1)
-    person2 = index.get_or_create_person(ns2)
-    assert person1 is person2
-    assert person2.has_name(ns1.name)
-    assert person2.has_name(ns2.name)
-    # Canonical name should still be the one defined in variants list
-    assert person2.canonical_name == ns1.name
-
-
-def test_similar_names_defined_in_variant_list(index):
-    index._load_variant_list()
+def test_similar_names_defined_in_people_index(index):
+    index._load_people_index()
     similar = index.similar.subset("pranav-a")
     assert similar == {"pranav-a", "pranav-anand"}
 
 
 def test_similar_names_through_same_canonical_name(index):
-    index._load_variant_list()
+    index._load_people_index()
     similar = index.similar.subset("yang-liu-ict")
     assert similar == {
-        "yang-liu-edinburgh",
         "yang-liu-icsi",
         "yang-liu-ict",
         "yang-liu-microsoft",
@@ -219,22 +125,20 @@ def test_get_person_coauthors_counter(index_with_full_anthology):
     person = index.get_by_name(Name("Kathleen", "Dahlgren"))[0]
     coauthors = index.find_coauthors_counter(person)
     assert len(coauthors) == 1
-    assert coauthors["joyce-mcdowell"] == 1
+    assert coauthors["unverified/joyce-mcdowell"] == 1
 
     person = index.get_by_name(Name("Preslav", "Nakov"))[0]
     coauthors = index.find_coauthors_counter(person)
     assert len(coauthors) == 2
-    assert coauthors["joyce-mcdowell"] == 0
-    assert coauthors["aline-villavicencio"] == 2
+    assert coauthors["unverified/joyce-mcdowell"] == 0
+    assert coauthors["unverified/aline-villavicencio"] == 2
 
 
 def test_get_by_namespec(index_with_full_anthology):
     index = index_with_full_anthology
     ns1 = NameSpecification(Name("Yang", "Liu"))
     ns2 = NameSpecification(Name("Yang", "Liu"), id="yang-liu-microsoft")
-    # In contrast to test_get_or_create_person_new_person_disallowed, this
-    # should behave differently because it makes sure the index is built first
-    with pytest.raises():
+    with pytest.raises(NameIDUndefinedError):
         index.get_by_namespec(ns1)
     person = index.get_by_namespec(ns2)
     assert person.id == "yang-liu-microsoft"
