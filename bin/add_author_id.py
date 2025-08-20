3271c3da2528aaed7ee0fb0393cd0ac344fe73a0
Matt Post <post@cs.jhu.edu>
2025-08-20 19:08:44 -0400

    Rework script to use module

--- bin/add_author_id.py
+++ bin/add_author_id.py
@@ -1,83 +1,72 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8  -*-
-#
-# Copyright 2022 Matt Post <post@cs.jhu.edu>
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# -*- coding: utf-8 -*-
+"""Add an author ID to NameSpecification entries using the acl_anthology module.
 
-"""
-Adds an ID tag to all instances of an author in all XML files where there is no ID tag.
-
-First use case was the Bill Byrne separation of July 2022.
-
-2020.gebnlp-1.4 E14-1026 E14-1028 W16-2324 2021.acl-long.55 2021.eancs-1.2 W15-0116 D19-1125 D19-1331 D19-1459 P14-3000 2022.naacl-main.136 W18-1821 W18-5420 W18-6427 2020.nlp4call-1.2 N19-1406 2021.emnlp-main.620 2021.emnlp-main.666 N18-2081 N18-3013 W17-3531 2020.wmt-1.94 D15-1273 2022.nlp4convai-1.7 P16-2049 C14-1195 P19-1022 W19-4417 W19-4424 W19-5340 W19-5421 2020.wat-1.21 E17-2058 2022.ecnlp-1.13 J14-3008 N15-1041 N15-1105 P18-2051 D17-1208 D17-1220 D17-2005 2020.acl-main.690 2020.acl-main.693 N16-1100 2022.findings-acl.223 2022.findings-acl.301
+This script finds author/editor name specifications matching a given
+first/last name where no explicit ID is present, sets the provided ID, and
+saves the affected collection XML files.
 
 Usage:
-
-    ./add_author_id.py bill-byrne --last-name Byrne --first-name Bill
+    ./add_author_id.py <id> --last-name <Last> [--first-name <First>] [--data-dir <path>]
 """
 
+from __future__ import annotations
+
 import argparse
-import os
-
-from pathlib import Path
-from anthology.utils import indent
+from collections import defaultdict
 from itertools import chain
+from typing import Set
 
-import lxml.etree as ET
+from acl_anthology.anthology import Anthology
+from acl_anthology.people import Name
 
 
-def main(args):
-    for xml_file in Path(args.data_dir).glob("**/*.xml"):
-        changed_one = False
+def main(args: argparse.Namespace) -> None:
+    anthology = Anthology(args.data_dir, verbose=True)
 
-        tree = ET.parse(xml_file)
-        for paper_xml in chain(
-            tree.getroot().findall(".//paper"), tree.getroot().findall(".//meta")
-        ):
-            for author_xml in chain(
-                paper_xml.findall("./author"), paper_xml.findall("./editor")
-            ):
-                if "id" in author_xml.attrib:
-                    continue
-                last_name = author_xml.find("./last").text
-                try:
-                    first_name = author_xml.find("./first").text
-                except AttributeError:
-                    first_name = ""
-                if last_name == args.last_name and first_name == args.first_name:
-                    paper_id = (
-                        paper_xml.attrib["id"] if paper_xml.text == "paper" else "0"
-                    )
-                    anth_id = f"{xml_file}/{paper_id}"
-                    print(f"Adding {args.id} to {anth_id}...")
-                    author_xml.attrib["id"] = args.id
-                    changed_one = True
+    last_name, first_name = args.name.split(",") if "," in args.name else (args.name, None)
 
-        if changed_one:
-            indent(tree.getroot())
-            tree.write(xml_file, encoding="UTF-8", xml_declaration=True)
+    people = anthology.find_people(args.name)
+    if not people:
+        print(f"No person found matching name {args.name}")
+
+    # find the person with the non-explicit ID
+    for person in people:
+        if not person.is_explicit:
+            break
+    print(f"Found person: {person}")
+
+    if not person:
+        print(f"No person found matching name {args.name} with an explicit ID")
+        return
+
+    for paper in person.papers():
+        print("PAPER", paper.full_id)
+        authors = paper.get_editors() if paper.is_frontmatter else paper.authors
+        for author in authors:
+            if author.name in person.names:
+                print("-> Found", author)
+                author.id = args.id
+        # collection_paper_map[paper.collection_id].append(paper.full_id)
+
+    # save the anthology (doesn't currently work)
+    anthology.save_all()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser("Add an author ID to all of an author's papers")
     parser.add_argument("id", help="Author ID to add")
-    parser.add_argument("--last-name", help="Author's last name")
-    parser.add_argument("--first-name", help="Author's first name")
-    parser.add_argument("--confirm", action="store_true", help="Confirm each instance")
+    parser.add_argument("--name", "-n", help="Author's name (last[, first])")
     parser.add_argument(
-        "--data-dir", default=os.path.join(os.path.dirname(__file__), "..", "data", "xml")
+        "--data-dir",
+        default=None,
+        help="Path to anthology data directory (default: ../data relative to repository root)",
     )
     args = parser.parse_args()
+    # Normalize data_dir to a Path string used by Anthology
+    # If the user supplies a path, trust it; otherwise compute relative to this script
+    if args.data_dir is None:
+        from pathlib import Path
+        args.data_dir = str(Path(__file__).parent.parent / "data")
 
     main(args)
