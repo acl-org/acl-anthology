9a5c60fb5469b647ad57e8942bbdf950f55a616e
Matt Post <mattpost@microsoft.com>
2026-01-23 09:31:25 -0500

    Comment out license pre-commit hook
    I also moved the copyright to the bottom of the license so it doesn't interfere with the header status, and added blank lines at the top and bottom for aesthetic reasons.

--- python/repl_with_anthology.py
+++ python/repl_with_anthology.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3 -i
 # -*- coding: utf-8 -*-
 #
-# Copyright 2024 Marcel Bollmann <marcel@bollmann.me>
-#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -14,6 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Copyright 2024 Marcel Bollmann <marcel@bollmann.me>
 
 ##############################################################################
 # This file is not meant to be run or imported directly, but via
