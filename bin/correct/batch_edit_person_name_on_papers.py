0bc9cabfd987c66629e0d76e96aec2c8fef988a4
Nathan Schneider <nathan.schneider@georgetown.edu>
2026-05-18 09:38:12 -0400

    Author/metadata corrections 2026-05-16 (#8270)
    * batch_edit_person_name_on_papers.py: fix CLI to avoid parsing issue; handle frontmatter
    * bulk_process_metadata.py: add --no-branch option
    
    * Process metadata corrections for 2022.acl-long.516 (closes #8267)
    * Process metadata corrections for 2022.findings-acl.165 (closes #8266)
    * Process metadata corrections for D18-1398 (closes #8265)
    * Process metadata corrections for D18-1035 (closes #8264)
    * Process metadata corrections for 2025.findings-emnlp.809 (closes #8263)
    * Process metadata corrections for 2026.findings-eacl.277 (closes #8261)
    * Process metadata corrections for W13-1731 (closes #8259)
    * Process metadata corrections for 2025.acl-long.1351 (closes #8252)
    * Process metadata corrections for Y96-1014 (closes #8251)
    * Process metadata corrections for 2023.findings-emnlp.104 (closes #8250)
    * Process metadata corrections for 2021.emnlp-main.493 (closes #8249)
    * Process metadata corrections for 2025.ijcnlp-long.77 (closes #8247)
    * Process metadata corrections for W17-3538 (closes #8246)
    * Process metadata corrections for W18-6551 (closes #8245)
    * Add middle initial across the board: Samuel R. Bowman
    * Add ORCID for author samuel-bowman (closes #8268)
    * Merge author victor-o-k-li (closes #8262)
    * Merge author danielle-s-mcnamara (closes #8258)
    * Process verification for marco-a-pretell/unverified (closes #8257)
    * Process metadata corrections for 2026.findings-eacl.324 (closes #8269)
    * Merge author jae-sung-lee (closes #8256)
    * Merge author m-dolores-jimenez-lopez (closes #8255)
    * Process metadata corrections for 2025.sigtyp-1.5 (closes #8254)
    * remove name variant with spurious split: m-dolores-jimenez-lopez (#8255)
    * Merge author jorge-carrillo-de-albornoz (closes #8253)
    * name disambiguation hints: jae-sung-lee, jaesung-lee-3510 (#8256)
    * jorge-carillo-de-albornoz: fix name split on paper (#8253)
    * Merge author jose-m-alonso (closes #8244)
    * Verify and disable name matching for author tong-wu (closes #8271)
    * Across the board renaming: Shay B Cohen -> Shay B. Cohen (#8273)
    * unlink paper and disable name matching: shay-b-cohen (#8273)
    * missing middle initial: Shay B. Cohen (#8273)
    * Merge author shay-b-cohen (closes #8273)
    * Process metadata corrections for 2025.r2lm-1.0 (closes #8278)
    * Process metadata corrections for 2025.lowresnlp-1.2 (closes #8277)
    * Process metadata corrections for 2026.eacl-demo.36 (closes #8275)
    * Process metadata corrections for 2025.ccl-2.44 (closes #8272)
    * Merge author rafael-munoz (closes #8274)
    * Merge author pranav-narayanan-venkit and ensure split is Pranav Narayanan | Venkit (closes #8279)
    * Process metadata corrections for 2023.emnlp-main.848 (closes #8280)
    * Merge author paloma-moreda-pozo and set name as Paloma Moreda across the board (closes #8281)
    * Merge author massih-r-amini and set name as Massih-Reza Amini across the board (closes #8284)
    * Merge author minh-quang-pham (closes #8285)
    * Process metadata corrections for 2024.uncertainlp-1.2 (closes #8282)

--- bin/correct/batch_edit_person_name_on_papers.py
+++ bin/correct/batch_edit_person_name_on_papers.py
@@ -1,21 +1,18 @@
 """
 Usage:
-  batch_edit_name_on_papers.py AUTHORID [OLDFIRST OLDLAST] FIRST LAST [--except PAPERID ... | --only PAPERID ...]
+  batch_edit_name_on_papers.py AUTHORID FIRST LAST [--old OLDFIRST OLDLAST] [--except PAPERIDS ... | --only PAPERIDS ...]
 
 Arguments:
     AUTHORID            ID of the author whose name is to be modified on papers.
-    OLDFIRST            Only update the name entry if it matches this first name.
-    OLDLAST             Only update the name entry if it matches this last name.
     FIRST               First name to apply to retrieved papers/volumes.
     LAST                Last name to apply to retrieved papers/volumes.
-    PAPERID             Paper or volume belonging to the author to specifically
-                        include or exclude (may list multiple IDs).
 
 Options:
     -h --help           Show this help message.
     --issue NUM         GitHub issue number to include in commit message.
-    --only PAPERID      Only update names on the specified paper(s)/volume(s).
-    --except PAPERID    Skip the specified paper(s)/volume(s) when updating names.
+    --old OLDFIRST OLDLAST    Only update the name if it matches OLDFIRST OLDLAST.
+    --only PAPERIDS      Only update names on the specified paper(s)/volume(s).
+    --except PAPERIDS    Skip the specified paper(s)/volume(s) when updating names.
 
 Apply the given first and last name strings to a Person across their papers,
 filtering by an existing name if OLDFIRST and OLDLAST are specified.
@@ -50,6 +47,8 @@ def batch_edit_names(
     person = anthology.get_person(author_id)
     assert person is not None, f"Could not find person: {author_id}"
 
+    # workaround for the fact that frontmatter and volumes share namespecs
+
     log.info(changes)
     if oldname:
         log.info(f"Limiting to instances of current name: {oldname}")
@@ -59,13 +58,21 @@ def batch_edit_names(
         assert ns.parent is not None
         item = ns.parent
         item_id = item.full_id
+        # note: frontmatter and Volumes share namespecs. So cannot simply check whether
+        # the full_id matches the user-provided ID
         if oldname and ns.name != oldname:
             skipped.append(item_id)
             continue
-        if item_id in exclude_paper_ids:
+        if any(
+            ns is anthology.get(paper_id).get_namespec_for(person)
+            for paper_id in exclude_paper_ids
+        ):
             skipped.append(item_id)
             continue
-        if specific_paper_ids and item_id not in specific_paper_ids:
+        if specific_paper_ids and not any(
+            ns is anthology.get(paper_id).get_namespec_for(person)
+            for paper_id in specific_paper_ids
+        ):
             skipped.append(item_id)
             continue
         targets.append(item_id)
@@ -76,7 +83,8 @@ def batch_edit_names(
     if specific_paper_ids:
         assert len(specific_paper_ids) == len(targets), specific_paper_ids
     elif exclude_paper_ids:
-        assert len(exclude_paper_ids) == len(skipped), exclude_paper_ids
+        assert len(exclude_paper_ids) <= len(skipped), exclude_paper_ids
+        # some could be skipped based on oldname
 
     anthology.save_all()
 
@@ -95,8 +103,9 @@ if __name__ == "__main__":
     with warnings.catch_warnings(action="ignore", category=NameSpecResolutionWarning):
         name = Name(first=args["FIRST"], last=args["LAST"])
         oldname = None
-        if args["OLDFIRST"]:
-            oldname = Name(first=args["OLDFIRST"], last=args["OLDLAST"])
+        if args["--old"]:
+            oldfirst, oldlast = args["--old"], args["OLDLAST"]
+            oldname = Name(first=oldfirst, last=oldlast)
         msg = batch_edit_names(
             author_id=args["AUTHORID"],
             name=name,
