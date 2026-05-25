5bbafc2030b0be5b2916d58c7aa433662945b6ea
Nathan Schneider <nathan.schneider@georgetown.edu>
2026-05-24 21:03:32 -0400

    debug duplication

--- bin/create_hugo_data.py
+++ bin/create_hugo_data.py
@@ -527,7 +527,7 @@ def export_events(anthology, builddir, dryrun):
         for vol_id in data["volumes"][1:]:
             vol = anthology.get(vol_id)
             for venue in vol.venues():
-                if venue.id not in main_vol_venues:
+                if (venue.acronym, venue.id) not in main_vol_venues:
                     other_venues.add((venue.acronym, venue.id))
         vol_venues = main_vol_venues + sorted(other_venues)
         data["vol_venues"] = [tuples[1] for tuples in vol_venues]
