bbde6457fbbac73f4af6f193da0b4a035376476f
Nathan Schneider <nathan.schneider@georgetown.edu>
2026-06-11 16:59:23 -0400

    bulk_process_simple_verifications.py: hints to add labels to non-simple issues

--- bin/correct/bulk_process_simple_verifications.py
+++ bin/correct/bulk_process_simple_verifications.py
@@ -138,6 +138,19 @@ class AnthologyMetadataUpdater:
         closed_issues = []
 
         for issue in issues:
+            # Flag non-simple issues that are not already labeled as such
+            issuetype = []
+            issuelabels = [label.name for label in issue.get_labels()]
+            if "[x] Split/disambiguate:" in issue.body:
+                issuetype.append('SPLIT')
+            if "[x] Merge profiles:" in issue.body:
+                issuetype.append('MERGE')
+            if "[x] Name change:" in issue.body:
+                issuetype.append('NAMECHANGE')
+            if issuetype and not any(lbl.startswith('meta:') for lbl in issuelabels):
+                log.warning(f"#{issue.number}: No meta:* label for issue type {'+'.join(issuetype)} {issue.html_url}")
+
+            # Skip verification if non-simple
             if not issue.title.lower().endswith("/unverified"):
                 continue
 
@@ -150,6 +163,7 @@ class AnthologyMetadataUpdater:
             if "[ ] Name change:" not in issue.body:
                 continue
 
+            # Process the issue if consistent with command line args
             self.stats["visited_issues"] += 1
             try:
                 if issue_ids and issue.number not in issue_ids:
