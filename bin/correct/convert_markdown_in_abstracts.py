e6a4c6a4ac6c5d62e56ef00c5de460ea19832439
Nathan Schneider <nathan.schneider@georgetown.edu>
2026-07-26 10:00:58 -0400

    remove test code

--- bin/correct/convert_markdown_in_abstracts.py
+++ bin/correct/convert_markdown_in_abstracts.py
@@ -51,36 +51,6 @@ def process_md_in_xml(text: str) -> str:
 
 
 
-
-def test_abstract():
-    text = "Since the rise of neural natural-language-to-code models (NL<tex-math>\\rightarrow</tex-math>Code) that can generate long expressions and statements rather than a single next-token, one of the major problems has been reliably evaluating their generated output. In this paper, we propose CodeBERTScore: an evaluation metric for code generation, which builds on BERTScore (Zhang et al., 2020). Instead of encoding only the generated tokens as in BERTScore, CodeBERTScore also encodes the natural language input preceding the generated code, thus modeling the consistency between the generated code and its given natural language context as well. We perform an extensive evaluation of CodeBERTScore across four programming languages. We find that CodeBERTScore achieves a higher correlation with human preference and with functional correctness than all existing metrics. That is, generated code that receives a higher score by CodeBERTScore is more likely to be preferred by humans, as well as to function correctly when executed. We release five language-specific pretrained models to use with our publicly available code. Our language-specific models have been downloaded more than **1,000,000** times from the Huggingface Hub. Our code and data are available at https://github.com/neulab/code-bert-score"
-    out = process_md_in_xml(text)
-    text2 = "Since the rise of neural natural-language-to-code models (NL<tex-math>\\rightarrow</tex-math>Code) that can generate long expressions and statements rather than a single next-token, one of the major problems has been reliably evaluating their generated output. In this paper, we propose CodeBERTScore: an evaluation metric for code generation, which builds on BERTScore (Zhang et al., 2020). Instead of encoding only the generated tokens as in BERTScore, CodeBERTScore also encodes the natural language input preceding the generated code, thus modeling the consistency between the generated code and its given natural language context as well. We perform an extensive evaluation of CodeBERTScore across four programming languages. We find that CodeBERTScore achieves a higher correlation with human preference and with functional correctness than all existing metrics. That is, generated code that receives a higher score by CodeBERTScore is more likely to be preferred by humans, as well as to function correctly when executed. We release five language-specific pretrained models to use with our publicly available code. Our language-specific models have been downloaded more than <b>1,000,000</b> times from the Huggingface Hub. Our code and data are available at https://github.com/neulab/code-bert-score"
-    assert text2==out
-    tree = etree.fromstring("<abstract>" + text2 + "</abstract>")
-    print(etree.tostring(tree))
-    mutext = to_markuptext(tree)
-    print(mutext.as_xml())
-    anthology = Anthology.from_within_repo()
-    paper = anthology.get('2023.emnlp-main.859')
-    print(paper.abstract.as_xml())
-    xml = paper.abstract.to_xml("abstract")
-    print(etree.tostring(xml))
-    paper.abstract = mutext
-    print(paper.abstract.as_xml())
-    xml = paper.abstract.to_xml("abstract")
-    print(etree.tostring(xml))
-    anthology.save_all()
-    anthology = Anthology.from_within_repo()
-    paper = anthology.get('2023.emnlp-main.859')
-    print(paper.abstract.as_xml())
-    xml = paper.abstract.to_xml("abstract")
-    print(etree.tostring(xml))
-    assert False
-
-
-test_abstract()
-
 anthology = Anthology.from_within_repo()
 
 
