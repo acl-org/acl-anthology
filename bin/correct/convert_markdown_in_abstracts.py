232249b660a8ca0849040313aafeb22718cd2c77
Nathan Schneider <nathan.schneider@georgetown.edu>
2026-07-25 21:10:55 -0400

    debugging

--- bin/correct/convert_markdown_in_abstracts.py
+++ bin/correct/convert_markdown_in_abstracts.py
@@ -28,6 +28,7 @@ def process_md_in_xml(text: str) -> str:
 
     # Markdown -> HTML
     html = md.render(protected_text).strip()
+    
 
     if html.startswith('<p>'):
         html = html[3:]
@@ -45,12 +46,22 @@ def process_md_in_xml(text: str) -> str:
     return html
 
 
-def test_abstract():
-    text = "<abstract>Since the rise of neural natural-language-to-code models (NL<tex-math>\\rightarrow</tex-math>Code) that can generate long expressions and statements rather than a single next-token, one of the major problems has been reliably evaluating their generated output. In this paper, we propose CodeBERTScore: an evaluation metric for code generation, which builds on BERTScore (Zhang et al., 2020). Instead of encoding only the generated tokens as in BERTScore, CodeBERTScore also encodes the natural language input preceding the generated code, thus modeling the consistency between the generated code and its given natural language context as well. We perform an extensive evaluation of CodeBERTScore across four programming languages. We find that CodeBERTScore achieves a higher correlation with human preference and with functional correctness than all existing metrics. That is, generated code that receives a higher score by CodeBERTScore is more likely to be preferred by humans, as well as to function correctly when executed. We release five language-specific pretrained models to use with our publicly available code. Our language-specific models have been downloaded more than **1,000,000** times from the Huggingface Hub. Our code and data are available at https://github.com/neulab/code-bert-score</abstract>"
-    print(process_md_in_xml(text))
-
-
 anthology = Anthology.from_within_repo()
+
+def test_abstract():
+    text = "Since the rise of neural natural-language-to-code models (NL<tex-math>\\rightarrow</tex-math>Code) that can generate long expressions and statements rather than a single next-token, one of the major problems has been reliably evaluating their generated output. In this paper, we propose CodeBERTScore: an evaluation metric for code generation, which builds on BERTScore (Zhang et al., 2020). Instead of encoding only the generated tokens as in BERTScore, CodeBERTScore also encodes the natural language input preceding the generated code, thus modeling the consistency between the generated code and its given natural language context as well. We perform an extensive evaluation of CodeBERTScore across four programming languages. We find that CodeBERTScore achieves a higher correlation with human preference and with functional correctness than all existing metrics. That is, generated code that receives a higher score by CodeBERTScore is more likely to be preferred by humans, as well as to function correctly when executed. We release five language-specific pretrained models to use with our publicly available code. Our language-specific models have been downloaded more than **1,000,000** times from the Huggingface Hub. Our code and data are available at https://github.com/neulab/code-bert-score"
+    out = process_md_in_xml(text)
+    tree = etree.fromstring("<abstract>" + out + "</abstract>")
+    print(etree.tostring(tree))
+    paper = anthology.get('2023.emnlp-main.859')
+    paper.abstract = tree
+    anthology.save_all()
+    assert False
+
+
+test_abstract()
+
+
 
 
 i = 0
