2d9831978d386b77dc2b71df971ca4212cac3e09
Matt Post <post@cs.jhu.edu>
2026-08-11 15:04:41 -0400

    black

--- bin/ingest.py
+++ bin/ingest.py
@@ -480,9 +480,7 @@ def ensure_venue(anthology: Anthology, venue_abbrev: str, venue_title: str) -> s
         )
     if venue_slug not in anthology.venues:
         print(f"Creating venue '{venue_abbrev}' ({venue_title}) slug {venue_slug}")
-        anthology.venues.create(
-            id=venue_slug, acronym=venue_abbrev, name=venue_title
-        )
+        anthology.venues.create(id=venue_slug, acronym=venue_abbrev, name=venue_title)
     return venue_slug
 
 
